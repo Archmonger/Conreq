496bbbe68d3ef4b736066410b0ab9deea1238479
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-10-20 12:33:43 -0700

    Set input value on completion of search event

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,12 +1,18 @@
-// Event
+// Search Event
 $("#content-search").bind("enterKey", function(e) {
     let parameters = $(this).val();
     window.location = "/search/?query=" + encodeURI(parameters);
 });
 
-// Event Listener
+// Search Event Listener
 $("#content-search").keyup(function(e) {
     if (e.keyCode == 13) {
         $(this).trigger("enterKey");
     }
 });
+
+// Set input value on completion of search event
+let searchParams = new URLSearchParams(window.location.search);
+if (window.location.pathname == "/search/" && searchParams.has("query")) {
+    $("#content-search").val(decodeURI(searchParams.get("query")));
+}
