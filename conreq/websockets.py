570d5d9f7b2f815c7048c89e5e66006437fb3ba3
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-11-02 00:45:28 -0800

    ws auto reconnect

--- conreq/websockets.py
+++ conreq/websockets.py
@@ -1,25 +1,29 @@
 import json
 
 from channels.consumer import AsyncConsumer
+from channels.exceptions import StopConsumer
 
 # from channels.db import database_sync_to_async
 # from django.contrib.auth import get_user_model
+# from pprint import pprint
 
 
 class CommandConsumer(AsyncConsumer):
     """tmp"""
 
     async def websocket_connect(self, event):
-        """tmp"""
+        """When the browser attempts to """
         print("connected", event)
         await self.send({"type": "websocket.accept"})
         tmp_json = json.dumps({"test": "testy"})
         await self.send({"type": "websocket.send", "text": tmp_json})
+        # pprint(self.scope)
 
     async def websocket_disconnect(self, event):
-        """tmp"""
+        """When the websocket connection closes."""
         print("disconnected", event)
+        raise StopConsumer
 
     async def websocket_receive(self, event):
-        """tmp"""
+        """When the browser attempts to communicate with the server."""
         print("received", event)
--- static/js/websocket_commands.js
+++ static/js/websocket_commands.js
@@ -1,24 +1,40 @@
-let loc = window.location;
-let ws_start = "";
-if (loc.protocol == "https:") {
-    ws_start = "wss://";
-} else {
-    ws_start = "ws://";
-}
-let endpoint = ws_start + loc.host;
-let command_socket = new WebSocket(endpoint);
+function connect() {
+    // Create the command socket
+    let loc = window.location;
+    let ws_start = "";
+    if (loc.protocol == "https:") {
+        ws_start = "wss://";
+    } else {
+        ws_start = "ws://";
+    }
+    let endpoint = ws_start + loc.host;
+    let command_socket = new WebSocket(endpoint);
 
-command_socket.onmessage = function(e) {
-    console.log("socket message received", e);
-    let command = JSON.parse(e.data);
-    console.log("socket message JSON contents", command);
-};
-command_socket.onopen = function(e) {
-    console.log("socket opened", e);
-};
-command_socket.onerror = function(e) {
-    console.log("socket error", e);
-};
-command_socket.onclose = function(e) {
-    console.log("socket closed", e);
-};
+    // Socket events
+    command_socket.onopen = function(e) {
+        console.log("socket opened", e);
+    };
+
+    command_socket.onmessage = function(e) {
+        console.log("socket message received", e);
+        let command = JSON.parse(e.data);
+        console.log("socket message JSON contents", command);
+    };
+
+    command_socket.onclose = function(e) {
+        console.log(
+            "Socket is closed. Reconnect will be attempted in 1 second.",
+            e.reason
+        );
+        setTimeout(function() {
+            connect();
+        }, 1000);
+    };
+
+    command_socket.onerror = function(err) {
+        console.error("Socket encountered error: ", err.message, "Closing socket");
+        command_socket.close();
+    };
+}
+
+connect();
