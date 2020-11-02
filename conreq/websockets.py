c54518265d0e0ffd3488bdddd7846d910a5a273e
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-11-02 00:58:31 -0800

    use generic json websocket prototype

--- conreq/websockets.py
+++ conreq/websockets.py
@@ -1,29 +1,20 @@
-import json
-
-from channels.consumer import AsyncConsumer
-from channels.exceptions import StopConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 # from channels.db import database_sync_to_async
 # from django.contrib.auth import get_user_model
 # from pprint import pprint
 
 
-class CommandConsumer(AsyncConsumer):
+class CommandConsumer(AsyncJsonWebsocketConsumer):
     """tmp"""
 
-    async def websocket_connect(self, event):
+    async def connect(self):
         """When the browser attempts to """
-        print("connected", event)
-        await self.send({"type": "websocket.accept"})
-        tmp_json = json.dumps({"test": "testy"})
-        await self.send({"type": "websocket.send", "text": tmp_json})
+        print("connected")
+        await self.accept()
+        await self.send_json({"test": "testy"})
         # pprint(self.scope)
 
-    async def websocket_disconnect(self, event):
-        """When the websocket connection closes."""
-        print("disconnected", event)
-        raise StopConsumer
-
-    async def websocket_receive(self, event):
+    async def receive_json(self, content, **kwargs):
         """When the browser attempts to communicate with the server."""
-        print("received", event)
+        print("received", content)
