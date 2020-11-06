011126faa5d1e8b3b3c9d2243ec7f13c540507d0
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-11-05 21:47:53 -0800

    conditional get middleware

--- conreq/settings.py
+++ conreq/settings.py
@@ -90,6 +90,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.http.ConditionalGetMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
