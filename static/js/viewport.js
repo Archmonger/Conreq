b6aba4427b7f99c388ec1997b348d91e1cb9af59
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-11-11 21:14:54 -0800

    JS masonry layout in preparation for infinite-scroll

--- static/js/viewport.js
+++ static/js/viewport.js
@@ -1,1 +1,11 @@
 new SimpleBar($(".viewport-container")[0]);
+
+$(".viewport-posters").masonry({
+    itemSelector: ".masonry-item",
+    gutter: 10,
+    horizontalOrder: true,
+    fitWidth: true,
+    transitionDuration: "0.8s",
+    stagger: "0.03s",
+});
+$(".viewport-posters").css("opacity", "1");
