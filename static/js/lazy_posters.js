277367d760d41a7d9361c0167ae2359200400ebf
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-11-13 01:48:12 -0800

    masonry and lazy loading configuration only for poster viewport

--- static/js/lazy_posters.js
+++ static/js/lazy_posters.js
@@ -18,38 +18,41 @@ element_ready(viewport_selector).then(function() {
     lazyloader.update();
 
     // Select the target node
-    let target = document.querySelector(viewport_selector);
+    let target = document.querySelector(".viewport");
 
     // Create an observer instance
     let observer = new MutationObserver(function() {
-        // Lazy load on page updates
-        lazyloader.update();
+        // Lazy load and masonry on poster viewport page updates
+        if (document.querySelector(".viewport-posters")) {
+            lazyloader.update();
 
-        // Delete old masonry items
-        let masonry_items = $(".viewport-posters > .masonry-item");
-        if (masonry_items.length > 300) {
-            // Delete half the elements, rounded down to the nearest row
-            let card_width = $(".masonry-item").width() + 10;
-            let viewport_width = $(".viewport").width();
-            let cards_per_row = Math.trunc((viewport_width + 10) / card_width);
-            scroll_node = previous_bottom_node;
-            masonry_grid
-                .masonry(
-                    "remove",
-                    masonry_items.slice(
-                        0,
-                        Math.floor(Math.floor(masonry_items.length / 2) / cards_per_row) * cards_per_row
+            // Delete old masonry items
+            let masonry_items = $(".viewport-posters > .masonry-item");
+            if (masonry_items.length > 300) {
+                // Delete half the elements, rounded down to the nearest row
+                let card_width = $(".masonry-item").width() + 10;
+                let viewport_width = $(".viewport").width();
+                let cards_per_row = Math.trunc((viewport_width + 10) / card_width);
+                scroll_node = previous_bottom_node;
+                masonry_grid
+                    .masonry(
+                        "remove",
+                        masonry_items.slice(
+                            0,
+                            Math.floor(Math.floor(masonry_items.length / 2) / cards_per_row) *
+                            cards_per_row
+                        )
                     )
-                )
-                .masonry("layout");
+                    .masonry("layout");
 
-            // Scroll to the previous bottom node
-            scroll_node.scrollIntoView(false);
-            $(".viewport-container")[0].scrollBy(0, 30 + 10);
-            console.log("Deleting excess masonry items!", scroll_node);
+                // Scroll to the previous bottom node
+                scroll_node.scrollIntoView(false);
+                $(".viewport-container")[0].scrollBy(0, 30 + 10);
+                console.log("Deleting excess masonry items!", scroll_node);
+            }
+            previous_bottom_node = masonry_items.last()[0];
+            console.log("Updating poster viewport");
         }
-        previous_bottom_node = masonry_items.last()[0];
-        console.log("Viewport changes observed!");
     });
 
     // Configuration of the observer
