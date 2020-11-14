9eafdf6ad61c8b0a11ce052680ec597caff6bf9e
Archmonger <16909269+Archmonger@users.noreply.github.com>
2020-11-13 20:36:11 -0800

    remove legacy app_name

--- conreq/apps/discover/urls.py
+++ conreq/apps/discover/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = "discover"
 urlpatterns = [
     path("", views.discover, name="index"),
 ]
--- conreq/apps/more_info/urls.py
+++ conreq/apps/more_info/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = "more_info"
 urlpatterns = [
     path("", views.more_info, name="index"),
 ]
