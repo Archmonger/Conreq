b097c0d40a2c5f3504cc2b70c3eb0430ececa899
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-13 02:01:48 -0800

    refactor asgi.py in preparation for daphne webserver

--- conreq/asgi.py
+++ conreq/asgi.py
@@ -9,19 +9,25 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conreq.settings")
+django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
 from conreq.server_websockets import CommandConsumer
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conreq.settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter([url("", CommandConsumer().as_asgi())]))
         ),
