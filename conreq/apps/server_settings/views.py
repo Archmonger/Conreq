e25a283a08eeda380a6fcf8a3c1d493e3468119c
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-18 19:51:42 -0800

    basic layout for dropdown items

--- conreq/apps/server_settings/views.py
+++ conreq/apps/server_settings/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
 from platform import platform
 
+from conreq.apps.server_settings.models import ConreqConfig
+from conreq.core.content_manager import ContentManager
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse
 from django.template import loader
@@ -12,6 +14,24 @@ from django.template import loader
 def server_settings(request):
     template = loader.get_template("viewport/server_settings.html")
 
-    context = {"os_platform": platform()}
+    # Database values
+    conreq_config = ConreqConfig.get_solo()
+
+    # Obtain sonarr and radarr information
+    content_manger = ContentManager()
+
+    # Sonarr Anime Quality Profiles
+    sonarr_quality_profiles = []
+    current_sonarr_quality_profile = ""
+    for profile in content_manger.sonarr_quality_profiles():
+        if conreq_config.sonarr_anime_quality_profile == profile["id"]:
+            current_sonarr_quality_profile = profile["name"]
+        sonarr_quality_profiles.append({"id": profile["id"], "label": profile["name"]})
+
+    context = {
+        "os_platform": platform(),
+        "sonarr_quality_profiles": sonarr_quality_profiles,
+        "current_sonarr_quality_profile": current_sonarr_quality_profile,
+    }
 
     return HttpResponse(template.render(context, request))
