d14f4f092975d9c23b86d336efd8906995fc72a2
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-20 19:08:49 -0800

    move static to conreq

--- conreq/settings.py
+++ conreq/settings.py
@@ -353,7 +353,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "static-deploy")
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "conreq", "static")]
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
