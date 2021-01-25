3a1617ce7169621f61ea64a9cdce6d6e02a2aa9a
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-24 22:59:50 -0800

    Revert "use jquery load"
    This reverts commit 16db5a153199c7cf0d85f86d6dcc58808d1516a1.

--- conreq/static/js/viewport.js
+++ conreq/static/js/viewport.js
@@ -240,7 +240,8 @@ let generate_viewport = function () {
   $(".viewport-container>.loading-animation-container").show();
 
   // Fetch the new content, display it, and hide the loading animation
-  $(".viewport-container").load(window_location, function () {
+  $.get(window_location, function (viewport_html) {
+    $(".viewport-container")[0].innerHTML = DOMPurify.sanitize(viewport_html);
     refresh_viewport();
     $(".viewport-container>.loading-animation-container").hide();
     $(".viewport-container>.viewport").css("opacity", "1");
