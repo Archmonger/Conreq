d749fe3a286cff53f0b33aabcc28c350fa63e5f4
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-27 16:19:06 -0800

    output user info to template

--- conreq/apps/manage_users/views.py
+++ conreq/apps/manage_users/views.py
@@ -2,11 +2,13 @@ from conreq.utils.apps import generate_context
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 @login_required
 @user_passes_test(lambda u: u.is_staff)
 def manage_users(request):
     template = loader.get_template("viewport/manage_users.html")
-    context = generate_context({})
+    users = get_user_model().objects.values()
+    context = generate_context({"users": users})
     return HttpResponse(template.render(context, request))
