7af6ad38a8980cc7a71f2d77daec908cd8f1c0e3
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-28 00:12:45 -0800

    generate_invite_code url complete

--- conreq/apps/sign_up/views.py
+++ conreq/apps/sign_up/views.py
@@ -1,13 +1,15 @@
 from secrets import token_hex
 
-from conreq.utils import cache
 from conreq.utils.apps import generate_context
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.cache import cache
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.views.decorators.cache import cache_page
 
+# Days, Hours, Minutes, Seconds
+INVITE_CODE_DURATION = 7 * 24 * 60 * 60
 
 # Create your views here.
 @cache_page(60 * 60)
@@ -15,8 +17,18 @@ def invite_code(request):
     pass
 
 
-@cache_page(1)
 @login_required
 @user_passes_test(lambda u: u.is_staff)
 def generate_invite_code(request):
-    pass
+    invite_code_created = False
+
+    # Create an invite code that isn't already active
+    while not invite_code_created:
+        new_code = token_hex(12)
+        cache_key = "invite_code" + new_code
+
+        if cache.get(cache_key) is None:
+            cache.set(cache_key, True, INVITE_CODE_DURATION)
+            invite_code_created = True
+
+    return JsonResponse({"invite_code": new_code})
