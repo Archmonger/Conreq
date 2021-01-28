86c507a54ef9a7ba7045f209241b21b5b84b7d01
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-27 18:44:43 -0800

    fix page transition animations

--- conreq/static/js/viewport.js
+++ conreq/static/js/viewport.js
@@ -245,7 +245,10 @@ var generate_viewport = function () {
     $(".viewport-container")[0].innerHTML = DOMPurify.sanitize(viewport_html);
     refresh_viewport();
     $(".viewport-container>.loading-animation-container").hide();
-    $(".viewport-container>.viewport").css("opacity", "1");
+
+    setTimeout(function () {
+      $(".viewport-container>.viewport").css("opacity", "1");
+    }, 10);
 
     // Add any click events needed
     add_event_listeners();
