c630de605407fa3aba96d491803baaf28fa12854
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-27 18:45:42 -0800

    small cache on manage_users and server_settings

--- conreq/apps/manage_users/views.py
+++ conreq/apps/manage_users/views.py
@@ -1,10 +1,13 @@
 from conreq.utils.apps import generate_context
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse
 from django.template import loader
-from django.contrib.auth import get_user_model
+from django.views.decorators.cache import cache_page
+
 
 # Create your views here.
+@cache_page(1)
 @login_required
 @user_passes_test(lambda u: u.is_staff)
 def manage_users(request):
--- conreq/apps/server_settings/views.py
+++ conreq/apps/server_settings/views.py
@@ -6,10 +6,12 @@ from conreq.utils import log
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.cache import cache_page
 
 __logger = log.get_logger(__name__)
 
 # Create your views here.
+@cache_page(1)
 @login_required
 @user_passes_test(lambda u: u.is_staff)
 def server_settings(request):
