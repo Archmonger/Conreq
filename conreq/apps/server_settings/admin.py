ef415b7b61149cd7f3c531b5b00f029878e702ff
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-29 20:29:51 -0800

    Add conreq config to the django admin page

--- conreq/apps/server_settings/admin.py
+++ conreq/apps/server_settings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from conreq.apps.server_settings.models import ConreqConfig
 
 # Register your models here.
+@admin.register(ConreqConfig)
+class ServerSettings(admin.ModelAdmin):
+    pass
