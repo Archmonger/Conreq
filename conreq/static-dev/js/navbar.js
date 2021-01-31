47f7cd8a7da4ca3e53a70d1824763389c73dfd6a
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-01-30 21:02:21 -0800

    dynamically obtain search URL

--- conreq/static-dev/js/navbar.js
+++ conreq/static-dev/js/navbar.js
@@ -1,14 +1,15 @@
 // Search Event Action
-$("#content-search").on("enterKey", function(e) {
-    let parameters = $(this).val();
-    window.location = "#/search/?query=" + encodeURI(parameters);
+$("#content-search").on("enterKey", function (e) {
+  let parameters = $(this).val();
+  window.location =
+    "#" + $(this).data("search-url") + "?query=" + encodeURI(parameters);
 });
 
 // Search Event Listener
-$("#content-search").keyup(function(e) {
-    if (e.keyCode == 13) {
-        $(this).trigger("enterKey");
-    }
+$("#content-search").keyup(function (e) {
+  if (e.keyCode == 13) {
+    $(this).trigger("enterKey");
+  }
 });
 
 // Slide in animations for navbar, sidebar, and viewport
