4b87cc41e4e9977f3d27e5fc4e9974ca0e90232e
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-02-02 23:37:03 -0800

    fix issue modal url

--- conreq/apps/issue_reporting/urls.py
+++ conreq/apps/issue_reporting/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "issue_reporting"
 urlpatterns = [
     path("", views.report_issue, name="report_issue"),
-    path("issue_modal", views.report_issue_modal, name="report_issue_modal"),
+    path("issue_modal/", views.report_issue_modal, name="report_issue_modal"),
 ]
