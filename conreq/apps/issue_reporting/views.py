8fd7b08022e8ececbb51588af09505aeec6e377a
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-02-02 22:04:12 -0800

    dynamically generate issue list

--- conreq/apps/issue_reporting/views.py
+++ conreq/apps/issue_reporting/views.py
@@ -1,12 +1,25 @@
 from conreq.core.content_discovery import ContentDiscovery
 from conreq.core.content_manager import ContentManager
-from conreq.utils import log
 from conreq.utils.apps import generate_context
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.cache import cache_page
 
+ISSUE_LIST = [
+    "Video does not match what was requested.",
+    "Video does not load.",
+    "Video does not exist.",
+    "Video is in the wrong category/folder.",
+    "Wrong video length.",
+    "Wrong audio language.",
+    "Wrong subtitle language.",
+    "Bad or corrupt video.",
+    "Bad or corrupt audio.",
+    "Bad subtitles.",
+    "Missing subtitles.",
+    "Other:",
+]
 
 # Create your views here.
 @login_required
@@ -19,10 +32,7 @@ def report_issue(request):
     content_type = request.GET.get("content_type", None)
 
     if tmdb_id and content_type:
-        # Fetch and process
-        content = content_discovery.get_by_tmdb_id(
-            tmdb_id, content_type, obtain_extras=False
-        )
+        pass
 
     elif tvdb_id and content_type:
         pass
@@ -32,7 +42,6 @@ def report_issue(request):
     return HttpResponse(template.render(context, request))
 
 
-# @cache_page(24 * 60 * 60)
 @login_required
 def report_issue_modal(request):
     content_discovery = ContentDiscovery()
@@ -51,6 +60,6 @@ def report_issue_modal(request):
     elif tvdb_id and content_type:
         pass
 
-    context = generate_context({})
+    context = generate_context({"issues": ISSUE_LIST})
     template = loader.get_template("modal/report_issue.html")
     return HttpResponse(template.render(context, request))
