7afcf69704eb5b13283f1bf9856f009fa7a96abd
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-02-02 23:39:16 -0800

    create ReportedIssue DB model

--- conreq/apps/issue_reporting/models.py
+++ conreq/apps/issue_reporting/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class ReportedIssue(models.Model):
+    name = models.CharField(max_length=255)
+    resolution = models.CharField(max_length=255)
+    reported_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_reported = models.DateTimeField(auto_now_add=True)
+
+    content_id = models.CharField(max_length=30)
+    source = models.CharField(max_length=30)
+    content_type = models.CharField(max_length=30)
+
+    seasons = models.TextField(blank=True)
+    episode_ids = models.TextField(blank=True)
