ec9f3165fb00a896b05b2eed797b40bb09fa63aa
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-02-07 00:47:41 -0800

    create abstract background_task method

--- conreq/apps/base/tasks.py
+++ conreq/apps/base/tasks.py
@@ -1,10 +1,15 @@
-from huey import crontab
-from huey.contrib.djhuey import periodic_task, db_task
 from conreq.core.content_manager import ContentManager
+from huey import crontab
+from huey.contrib.djhuey import db_task, db_periodic_task
 
 
-@db_task()
-@periodic_task(crontab(minute="*/1"))
+@db_periodic_task(crontab(minute="*/1"), name="refresh_content")
 def refresh_content():
     """Checks Sonarr/Radarr for new entries every minute."""
     ContentManager().refresh_content()
+
+
+@db_task(name="background_task")
+def background_task(function, *args, **kwargs):
+    """Adds any function to the background task queue."""
+    function(*args, **kwargs)
