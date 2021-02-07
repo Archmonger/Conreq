2cf4f11c508d4e2bae3961555e6fca019b055f30
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-02-07 01:38:34 -0800

    remove huey names

--- conreq/apps/base/tasks.py
+++ conreq/apps/base/tasks.py
@@ -3,13 +3,13 @@ from huey import crontab
 from huey.contrib.djhuey import db_task, db_periodic_task
 
 
-@db_periodic_task(crontab(minute="*/1"), name="refresh_content")
+@db_periodic_task(crontab(minute="*/1"))
 def refresh_content():
     """Checks Sonarr/Radarr for new entries every minute."""
     ContentManager().refresh_content()
 
 
-@db_task(name="background_task")
+@db_task()
 def background_task(function, *args, **kwargs):
     """Adds any function to the background task queue."""
     function(*args, **kwargs)
