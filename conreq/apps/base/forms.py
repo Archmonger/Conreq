99fb304b1de89ac60d4690fef97c6ada8a04d4fd
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-02-19 18:07:38 -0800

    fix conreq_config admin page

--- conreq/apps/base/forms.py
+++ conreq/apps/base/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.db.models import URLField
+from django.forms import URLField
 
 from . import validators
 
