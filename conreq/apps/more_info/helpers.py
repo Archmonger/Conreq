fbbee3f65f70329c6c76a213ce67449060c10ad9
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-02-24 20:21:35 -0800

    don't use len to check existance

--- conreq/apps/more_info/helpers.py
+++ conreq/apps/more_info/helpers.py
@@ -35,7 +35,7 @@ def preprocess_arr_result(arr_result):
     if (
         arr_result.__contains__("overview")
         and isinstance(arr_result["overview"], str)
-        and len(arr_result["overview"]) == 0
+        and not arr_result["overview"]
     ):
         arr_result["overview"] = None
     # Runtime
@@ -43,7 +43,7 @@ def preprocess_arr_result(arr_result):
         arr_result["runtime"] = "{:d}h {:d}m".format(*divmod(arr_result["runtime"], 60))
     # Release Status
     if arr_result.__contains__("status") and isinstance(arr_result["status"], str):
-        if len(arr_result["status"]) == 0:
+        if not arr_result["status"]:
             arr_result["status"] = None
         else:
             arr_result["status"] = arr_result["status"].capitalize()
@@ -51,27 +51,27 @@ def preprocess_arr_result(arr_result):
     if (
         arr_result.__contains__("genres")
         and isinstance(arr_result["genres"], list)
-        and len(arr_result["genres"]) == 0
+        and not arr_result["genres"]
     ):
         arr_result["genres"] = None
     # Networks
     if (
         arr_result.__contains__("network")
         and isinstance(arr_result["network"], list)
-        and len(arr_result["network"]) != 0
+        and arr_result["network"]
     ):
         arr_result["networks"] = arr_result["network"]
     if (
         arr_result.__contains__("network")
         and isinstance(arr_result["network"], str)
-        and len(arr_result["network"]) != 0
+        and arr_result["network"]
     ):
         arr_result["networks"] = [arr_result["network"]]
     # Backdrop
     if (
         arr_result.__contains__("images")
         and isinstance(arr_result["images"], list)
-        and len(arr_result["images"]) != 0
+        and arr_result["images"]
     ):
         backdrop = is_key_value_in_list(
             "coverType", "fanart", arr_result["images"], return_item=True
@@ -87,18 +87,18 @@ def preprocess_tmdb_result(tmdb_result):
     if (
         tmdb_result.__contains__("overview")
         and isinstance(tmdb_result["overview"], str)
-        and len(tmdb_result["overview"]) == 0
+        and not tmdb_result["overview"]
     ):
         tmdb_result["overview"] = None
     # Budget
     if tmdb_result.__contains__("budget") and isinstance(tmdb_result["budget"], int):
-        if tmdb_result["budget"] == 0:
+        if not tmdb_result["budget"]:
             tmdb_result["budget"] = None
         else:
             tmdb_result["budget"] = "{:,}".format(tmdb_result["budget"])
     # Revenue
     if tmdb_result.__contains__("revenue") and isinstance(tmdb_result["revenue"], int):
-        if tmdb_result["revenue"] == 0:
+        if not tmdb_result["revenue"]:
             tmdb_result["revenue"] = None
         else:
             tmdb_result["revenue"] = "{:,}".format(tmdb_result["revenue"])
@@ -113,7 +113,7 @@ def preprocess_tmdb_result(tmdb_result):
         and tmdb_result["reviews"].__contains__("results")
         and isinstance(tmdb_result["reviews"]["results"], list)
     ):
-        if len(tmdb_result["reviews"]["results"]) == 0:
+        if not tmdb_result["reviews"]["results"]:
             tmdb_result["reviews"]["results"] = None
         else:
             for review in tmdb_result["reviews"]["results"]:
@@ -125,7 +125,7 @@ def preprocess_tmdb_result(tmdb_result):
         tmdb_result.__contains__("keywords")
         and tmdb_result["keywords"].__contains__("results")
         and isinstance(tmdb_result["keywords"]["results"], list)
-        and len(tmdb_result["keywords"]["results"]) == 0
+        and not tmdb_result["keywords"]["results"]
     ):
         tmdb_result["keywords"]["results"] = None
     # Cast Members
@@ -133,7 +133,7 @@ def preprocess_tmdb_result(tmdb_result):
         tmdb_result.__contains__("credits")
         and tmdb_result["credits"].__contains__("cast")
         and isinstance(tmdb_result["credits"]["cast"], list)
-        and len(tmdb_result["credits"]["cast"]) == 0
+        and not tmdb_result["credits"]["cast"]
     ):
         tmdb_result["credits"]["cast"] = None
     # Videos
@@ -142,7 +142,7 @@ def preprocess_tmdb_result(tmdb_result):
         and tmdb_result["videos"].__contains__("results")
         and isinstance(tmdb_result["videos"]["results"], list)
     ):
-        if len(tmdb_result["videos"]["results"]) == 0:
+        if not tmdb_result["videos"]["results"]:
             tmdb_result["videos"]["results"] = None
         else:
             contains_youtube = False
@@ -157,14 +157,14 @@ def preprocess_tmdb_result(tmdb_result):
         tmdb_result.__contains__("images")
         and tmdb_result["images"].__contains__("backdrops")
         and isinstance(tmdb_result["images"]["backdrops"], list)
-        and len(tmdb_result["images"]["backdrops"]) == 0
+        and not tmdb_result["images"]["backdrops"]
     ):
         tmdb_result["images"]["backdrops"] = None
     # Last Air Date
     if (
         tmdb_result.__contains__("last_air_date")
         and isinstance(tmdb_result["last_air_date"], str)
-        and len(tmdb_result["last_air_date"]) != 0
+        and tmdb_result["last_air_date"]
     ):
         year, month, day = tmdb_result["last_air_date"].split(sep="-")
         month = month_name[int(month)]
@@ -173,7 +173,7 @@ def preprocess_tmdb_result(tmdb_result):
     if (
         tmdb_result.__contains__("first_air_date")
         and isinstance(tmdb_result["first_air_date"], str)
-        and len(tmdb_result["first_air_date"]) != 0
+        and tmdb_result["first_air_date"]
     ):
         year, month, day = tmdb_result["first_air_date"].split(sep="-")
         month = month_name[int(month)]
@@ -194,7 +194,7 @@ def preprocess_tmdb_result(tmdb_result):
     if (
         tmdb_result.__contains__("release_date")
         and isinstance(tmdb_result["release_date"], str)
-        and len(tmdb_result["release_date"]) != 0
+        and tmdb_result["release_date"]
     ):
         year, month, day = tmdb_result["release_date"].split(sep="-")
         month = month_name[int(month)]
@@ -203,7 +203,7 @@ def preprocess_tmdb_result(tmdb_result):
     if (
         tmdb_result.__contains__("backdrop_path")
         and isinstance(tmdb_result["backdrop_path"], str)
-        and len(tmdb_result["backdrop_path"]) != 0
+        and tmdb_result["backdrop_path"]
         and tmdb_result["backdrop_path"].find(TMDB_BACKDROP_URL) == -1
     ):
         tmdb_result["backdrop_path"] = TMDB_BACKDROP_URL + tmdb_result["backdrop_path"]
@@ -211,7 +211,7 @@ def preprocess_tmdb_result(tmdb_result):
     if (
         tmdb_result.__contains__("poster_path")
         and isinstance(tmdb_result["poster_path"], str)
-        and len(tmdb_result["poster_path"]) != 0
+        and tmdb_result["poster_path"]
         and tmdb_result["poster_path"].find(TMDB_POSTER_URL) == -1
     ):
         tmdb_result["poster_path"] = TMDB_POSTER_URL + tmdb_result["poster_path"]
