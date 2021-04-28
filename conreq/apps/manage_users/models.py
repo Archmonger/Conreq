7807f1ca5d42bb2b0ad0168918ff07294e0c1f5d
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-04-28 16:48:39 -0700

    dynamically get user model

--- conreq/apps/manage_users/models.py
+++ conreq/apps/manage_users/models.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from model_utils import FieldTracker
 
+User = get_user_model()
 
 # Create your models here.
 class Profile(models.Model):
