f0c06d0f08de4346f98a2a9500f36bed43a4a477
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-06-13 00:41:57 -0700

    fix unclickable "read more" btn

--- conreq/asgi.py
+++ conreq/asgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 
 from django.conf.urls import url
-
 from django.core.asgi import get_asgi_application
 
 # Fetch ASGI application before importing dependencies that require ORM models.
