23aa77079f773451873173500b94f365dfcfd401
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-07-25 04:42:01 -0700

    verbose logging for failed sonarr/radarr

--- conreq/core/user_requests/helpers.py
+++ conreq/core/user_requests/helpers.py
@@ -16,62 +16,97 @@ _logger = log.get_logger(__name__)
 
 def radarr_request_background_task(tmdb_id, content_manager, radarr_params, username):
     """Function that can be run in the background to request something on Radarr"""
-    # Check if the movie is already within Radarr's collection
-    movie = content_manager.get(tmdb_id=tmdb_id)
+    try:
+        # Check if the movie is already within Radarr's collection
+        movie = content_manager.get(tmdb_id=tmdb_id)
 
-    # If it doesn't already exists, add then request it
-    if movie is None:
-        movie = content_manager.add(
-            tmdb_id=tmdb_id,
-            quality_profile_id=radarr_params["radarr_profile_id"],
-            root_dir=radarr_params["radarr_root"],
+        # If it doesn't already exists, add then request it
+        if movie is None:
+            movie = content_manager.add(
+                tmdb_id=tmdb_id,
+                quality_profile_id=radarr_params["radarr_profile_id"],
+                root_dir=radarr_params["radarr_root"],
+            )
+
+        # Request
+        content_manager.request(radarr_id=movie["id"])
+
+        username = username if username else "API"
+        log.handler(
+            username + " requested movie " + movie["title"],
+            log.INFO,
+            _logger,
         )
-
-    # Request
-    content_manager.request(radarr_id=movie["id"])
-
-    username = username if username else "API"
-    log.handler(
-        username + " requested movie " + movie["title"],
-        log.INFO,
-        _logger,
-    )
+    except:
+        if "movie" not in locals():
+            movie = {"title": "Conreq Uninitialized"}
+        log.handler(
+            "\n".join(
+                (
+                    "Failed to request on Radarr!",
+                    f"tmdb_id: {tmdb_id}",
+                    f"radarr_params: {radarr_params}",
+                    f"movie: {movie}",
+                    f"username: {username}",
+                )
+            ),
+            log.ERROR,
+            _logger,
+        )
 
 
 def sonarr_request_background_task(
-    tvdb_id, request_parameters, content_manager, sonarr_params, username
+    tvdb_id, request_params, content_manager, sonarr_params, username
 ):
     """Function that can be run in the background to request something on Sonarr"""
-    # Check if the show is already within Sonarr's collection
-    show = content_manager.get(tvdb_id=tvdb_id)
+    try:
+        # Check if the show is already within Sonarr's collection
+        show = content_manager.get(tvdb_id=tvdb_id)
 
-    # If it doesn't already exists, add then request it
-    if show is None:
-        show = content_manager.add(
-            tvdb_id=tvdb_id,
-            quality_profile_id=sonarr_params["sonarr_profile_id"],
-            root_dir=sonarr_params["sonarr_root"],
-            series_type=sonarr_params["series_type"],
-            season_folders=sonarr_params["season_folders"],
+        # If it doesn't already exists, add then request it
+        if show is None:
+            show = content_manager.add(
+                tvdb_id=tvdb_id,
+                quality_profile_id=sonarr_params["sonarr_profile_id"],
+                root_dir=sonarr_params["sonarr_root"],
+                series_type=sonarr_params["series_type"],
+                season_folders=sonarr_params["season_folders"],
+            )
+
+        # Request
+        content_manager.request(
+            sonarr_id=show["id"],
+            seasons=request_params.get("seasons"),
+            episode_ids=request_params.get("episode_ids"),
         )
 
-    # Request
-    content_manager.request(
-        sonarr_id=show["id"],
-        seasons=request_parameters.get("seasons"),
-        episode_ids=request_parameters.get("episode_ids"),
-    )
-
-    username = username if username else "API"
-    log.handler(
-        username + " requested TV series " + show["title"],
-        log.INFO,
-        _logger,
-    )
+        username = username if username else "API"
+        log.handler(
+            username + " requested TV series " + show["title"],
+            log.INFO,
+            _logger,
+        )
+    except:
+        if "show" not in locals():
+            show = {"title": "Conreq Uninitialized"}
+        log.handler(
+            "\n".join(
+                (
+                    "Failed to request on Radarr!",
+                    f"tvdb_id: {tvdb_id}",
+                    f"request_params: {request_params}",
+                    f"sonarr_params: {sonarr_params}",
+                    f"show: {show}",
+                    f"username: {username}",
+                )
+            ),
+            log.ERROR,
+            _logger,
+        )
 
 
 def sonarr_request(
-    tvdb_id, tmdb_id, request, request_parameters, content_manager, content_discovery
+    tvdb_id, tmdb_id, request, request_params, content_manager, content_discovery
 ):
     """Request on Sonarr and save request history item to DB"""
     sonarr_params = obtain_sonarr_parameters(
@@ -82,7 +117,7 @@ def sonarr_request(
     background_task(
         sonarr_request_background_task,
         tvdb_id,
-        request_parameters,
+        request_params,
         content_manager,
         sonarr_params,
         request.user.username,
