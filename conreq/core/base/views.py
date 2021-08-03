a5bf8f3434d0b6dcc8e608de0449c92ad0fa8ce1
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-08-03 02:33:31 -0700

    Revert "Asynchronously load the main view"
    This reverts commit 3c6becda65bf8f7174f1b777de65f099930523d2.

--- conreq/core/base/views.py
+++ conreq/core/base/views.py
@@ -1,7 +1,7 @@
-from channels.db import database_sync_to_async
 from conreq.core.base.forms import InitializationForm
 from conreq.core.server_settings.models import ConreqConfig
 from conreq.utils.generic import get_base_url, get_debug_from_env
+from conreq.utils.testing import performance_metrics
 from django.contrib.auth import authenticate, get_user_model, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -14,13 +14,12 @@ base_url = get_base_url()
 debug = get_debug_from_env()
 
 
-# @performance_metrics()
-async def main(request):
+@performance_metrics()
+def main(request):
     """The primary view that handles whether to take the user to
     login, splash, initialization, or homepage."""
-    conreq_config = await database_sync_to_async(ConreqConfig.get_solo)()
-    user_model = await database_sync_to_async(get_user_model)()
-    user_objects = user_model.objects
+    conreq_config = ConreqConfig.get_solo()
+    user_objects = get_user_model().objects
 
     # Authenticate using Organizr headers
     organizr_username = request.headers.get("X-WEBAUTH-USER")
@@ -28,10 +27,9 @@ async def main(request):
         # Configure the user parameters
         organizr_email = request.headers.get("X-WEBAUTH-EMAIL")
         organizr_group = int(request.headers.get("X-WEBAUTH-GROUP"))
-        get_or_create_user = await database_sync_to_async(user_objects.get_or_create)(
+        user = user_objects.get_or_create(
             username=organizr_username,
-        )
-        user = get_or_create_user[0]
+        )[0]
         user.email = organizr_email
         user.is_staff = False
         if organizr_group == 0:
@@ -40,14 +38,14 @@ async def main(request):
             user.is_staff = True
         if user.has_usable_password():
             user.set_unusable_password()
-        await database_sync_to_async(user.save)()
+        user.save()
 
         # Make sure the user is labeled as a HTTP auth user
         if not user.profile.externally_authenticated:
             user.profile.externally_authenticated = True
-            await database_sync_to_async(user.save)()
+            user.save()
 
-        await database_sync_to_async(login)(request, user)
+        login(request, user)
 
     # Run the first time initialization if needed
     if conreq_config.conreq_initialized is False:
@@ -58,32 +56,28 @@ async def main(request):
             form = InitializationForm(request.POST)
 
             # Create the superuser and set up the database if the form is valid
-            if await database_sync_to_async(form.is_valid)():
-                await database_sync_to_async(form.save)()
+            if form.is_valid():
+                form.save()
                 username = form.cleaned_data.get("username")
                 password = form.cleaned_data.get("password1")
-                user = await database_sync_to_async(authenticate)(
-                    username=username, password=password
-                )
+                user = authenticate(username=username, password=password)
                 user.is_staff = True
                 user.is_admin = True
                 user.is_superuser = True
-                await database_sync_to_async(user.save)()
-                await database_sync_to_async(login)(request, user)
-                await database_sync_to_async(initialize_conreq)(conreq_config, form)
+                user.save()
+                login(request, user)
+                initialize_conreq(conreq_config, form)
                 return redirect("base:index")
 
             # Form data wasn't valid, so return the error codes
             template = loader.get_template("registration/initialization.html")
-            return HttpResponse(
-                await database_sync_to_async(template.render)({"form": form}, request)
-            )
+            return HttpResponse(template.render({"form": form}, request))
 
         # User needs to fill out the first time setup
         template = loader.get_template("registration/initialization.html")
-        return HttpResponse(await database_sync_to_async(template.render)({}, request))
+        return HttpResponse(template.render({}, request))
 
     # Render the base
-    return await database_sync_to_async(login_required(render))(
+    return login_required(render)(
         request, "primary/base_app.html", {"base_url": base_url, "debug": debug}
     )
