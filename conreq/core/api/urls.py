33bd91f418016d3a305d7db736323ee2475060d6
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-08-06 04:16:41 -0700

    clean up api endpoints in preparation for apps

--- conreq/core/api/urls.py
+++ conreq/core/api/urls.py
@@ -4,10 +4,31 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from . import views
 
 urlpatterns = [
-    path("request/tmdb", views.stub),  # GET
-    path("request/tmdb/<request_id>", views.stub),  # GET/DELETE
-    path("request/tmdb/tv/<tmdb_id>", views.RequestTv.as_view()),  # POST
-    path("request/tmdb/movie/<tmdb_id>", views.RequestMovie.as_view()),  # POST
+    # Default Endpoints
+    path("user", views.stub),  # GET/PUT
+    path("user/me", views.stub),  # GET/PUT
+    path("user/<username>", views.stub),  # GET/PUT/DELETE
+    path("auth/local", views.LocalAuthentication.as_view()),  # POST
+    path("auth/local/token", ObtainAuthToken.as_view()),  # POST/DELETE
+    path("system", views.stub),  # GET
+    path("system/settings", views.stub),  # GET/POST
+    # TMDB Requests App
+    path("tmdb/request", views.stub),  # GET
+    path("tmdb/request/<request_id>", views.stub),  # GET/DELETE
+    path("tmdb/request/tv/<tmdb_id>", views.RequestTv.as_view()),  # POST
+    path("tmdb/request/movie/<tmdb_id>", views.RequestMovie.as_view()),  # POST
+    path("tmdb/issue", views.stub),  # GET/POST
+    path("tmdb/issue/<issue_id>", views.stub),  # GET/DELETE
+    path("user/<username>/tmdb/issues", views.stub),  # GET
+    path("user/<username>/tmdb/requests", views.stub),  # GET
+    # Sonarr/Radarr Connection App
+    path("sonarr/library", views.stub),  # GET
+    path("sonarr/settings", views.stub),  # GET/POST
+    path("radarr/library", views.stub),  # GET
+    path("radarr/settings", views.stub),  # GET/POST
+    path("system/settings/sonarr", views.stub),  # GET/POST
+    path("system/settings/radarr", views.stub),  # GET/POST
+    # TMDB Connection App
     path("tmdb/discover", views.stub),  # GET
     path("tmdb/discover/tv", views.stub),  # GET
     path("tmdb/discover/tv/filters", views.stub),  # GET
@@ -30,19 +51,4 @@ urlpatterns = [
     path("tmdb/studio/<studio_id>", views.stub),  # GET
     path("tmdb/network/<network_id>", views.stub),  # GET
     path("tmdb/collection/<collection_id>", views.stub),  # GET
-    path("issue/tmdb", views.stub),  # GET/POST
-    path("issue/tmdb/<issue_id>", views.stub),  # GET/DELETE
-    path("user", views.stub),  # GET/PUT
-    path("user/me", views.stub),  # GET/PUT
-    path("user/<username>", views.stub),  # GET/PUT/DELETE
-    path("user/<username>/issues", views.stub),  # GET/POST
-    path("user/<username>/requests", views.stub),  # GET
-    path("auth/local", views.LocalAuthentication.as_view()),  # POST
-    path("auth/local/token", ObtainAuthToken.as_view()),  # POST/DELETE
-    path("system", views.stub),  # GET
-    path("settings", views.stub),  # GET/POST
-    path("settings/sonarr", views.stub),  # GET/POST
-    path("settings/radarr", views.stub),  # GET/POST
-    path("services/sonarr/library", views.stub),  # GET
-    path("services/radarr/library", views.stub),  # GET
 ]
