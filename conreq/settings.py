775952723af25a3fe4328d0c4d2d40b6cca4caea
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-08-17 00:14:45 -0700

    v0.20.13

--- conreq/settings.py
+++ conreq/settings.py
@@ -62,7 +62,7 @@ BASE_URL = get_base_url()
 
 
 # Application Settings
-DJVERSION_VERSION = "0.20.12"
+DJVERSION_VERSION = "0.20.13"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 SILKY_AUTHENTICATION = True
 SILKY_AUTHORISATION = True
