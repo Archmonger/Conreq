5ea78f712fa48daaf6a93ccf4c3b5f79a6566d39
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-08-19 22:34:34 -0700

    clean up the way setting script works

--- conreq/app/register/startup.py
+++ conreq/app/register/startup.py
@@ -24,23 +24,16 @@ def pre_startup():
             return _wrapped_func(*args, **kwargs)
 
 
-def setting(name, value):
+def setting(name: str, value: any):
     """Creates or modifies a variable in settings.py"""
     pass
 
 
-def setting_script():
-    """Runs a function in settings.py. Uses inspect.getsource(foo) and exec().
-    Function must have no args.
-    Appends `func.__name__()` at the end of the script.
-    See django-split-settings for inspiration:
-    https://github.com/sobolevn/django-split-settings/blob/872cf12005e6eb1323c302b5e37d190d4610f50a/split_settings/tools.py#L106
+def setting_script(file_path: str):
+    """Runs a file in settings.py.
+    Directly connect this to django-split-settings.
     """
-
-    def decorator(func):
-        @wraps(func)
-        def _wrapped_func(*args, **kwargs):
-            return _wrapped_func(*args, **kwargs)
+    pass
 
 
 def installed_app(path: str):
@@ -58,21 +51,26 @@ def middleware(
     pass
 
 
-def landing_page(template):
+def landing_page(template: str):
     """Changes the landing page variable in settings.py"""
     pass
 
 
-def sign_up_page(template):
+def home_page(template: str):
+    """Changes the home page variable in settings.py"""
+    pass
+
+
+def sign_up_page(template: str):
     """Changes the sign up page variable in settings.py"""
     pass
 
 
-def sign_in_page(template):
+def sign_in_page(template: str):
     """Changes the sign in page variable in settings.py"""
     pass
 
 
-def home_page(template):
-    """Changes the home page variable in settings.py"""
+def password_reset_page(template: str):
+    """Changes the password reset page variable in settings.py"""
     pass
