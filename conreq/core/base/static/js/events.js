151f465ff95e0a18d1e13959281b0eeb938479a1
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-08-25 09:02:07 -0700

    events and generic refactoring

--- conreq/core/base/static/js/events.js
+++ conreq/core/base/static/js/events.js
@@ -1,38 +1,13 @@
-let modal_loaded = false;
+var show_modal_loader = async function () {
+	$("#modal-container .loading-animation").show();
+	$("#modal-content").hide();
+	$("#modal-container").modal("show");
+};
 
-// TODO: Reassess
-var generate_modal = async function (modal_url) {
-	let modal_dialog = $("#modal-dialog");
-	// Fetch the series modal
-	modal_loaded = false;
-	get_url(modal_url, async function (modal_html) {
-		// Save that the modal was successfully loaded
-		modal_loaded = true;
-
-		// Place the new HTML on the page
-		modal_dialog[0].innerHTML = modal_html;
-
-		// Show the new content
-		$("#modal-container .loading-animation").hide();
-		$("#modal-content").show();
-		$("#modal-container").modal("show");
-	}).fail(async function () {
-		// Server couldn't fetch the modal
-		if (http_request.statusText != "abort") {
-			conreq_no_response_toast_message();
-		}
-		$("#modal-container").modal("hide");
-	});
-
-	// If the modal is taking too long to load, show a loading animation
-	setTimeout(async function () {
-		if (!modal_loaded) {
-			// Show the loading icon
-			$("#modal-content").hide();
-			$("#modal-container").modal("show");
-			$("#modal-container .loading-animation").show();
-		}
-	}, 300);
+var show_modal = async function () {
+	$("#modal-container .loading-animation").hide();
+	$("#modal-content").show();
+	$("#modal-container").modal("show");
 };
 
 // TODO: Reassess
@@ -50,43 +25,38 @@ var sidebar_collapse = async function () {
 	});
 };
 
-// TODO: Remove this
-var manage_user_btn = async function () {
-	$(".manage-user-btn").unbind("click");
-	$(".manage-user-btn").click(async function () {
-		let params = {
-			username: escape($(this).data("username")),
-		};
-		generate_modal($(this).data("modal-url") + "?" + $.param(params));
-	});
-};
-
-// TODO: Remove this
-$(document).ready(async function () {
-	$(".viewport-container").on("loaded", async function () {
-		// Bootstrap Table
-		if ($("table").length) {
-			$("table").bootstrapTable();
-		}
-	});
-
-	$(".viewport-container").on("destroy", function () {
-		// Bootstrap Table
-		if ($("table").length) {
-			$("table").bootstrapTable("destroy");
-		}
-	});
-});
-
 // TODO: Reassess
 $(document).ready(async function () {
 	if ($("#sidebar")[0]) {
 		new SimpleBar($("#sidebar")[0]);
 		sidebar_collapse();
-		manage_user_btn();
 	}
 });
 
+// Shows a toast message
 var toast_message = async function (parameters) {
 	iziToast.show(parameters);
+};
+
+// Hide any floaty objects
+var hide_popups = async function () {
+	$("#modal-container").modal("hide");
+	if (window.matchMedia("(max-width: 800px)").matches) {
+		if (!$("#sidebar").hasClass("collapsed")) {
+			$("#sidebar").addClass("collapsed");
+		}
+	}
+};
+
+// Creates a page element that copy_to_clipboard can copy from
+var create_invite_link_elem = function (invite_link) {
+	const el = document.createElement("textarea");
+	el.value = invite_link;
+	el.textContent = invite_link;
+	el.readOnly = true;
+	el.style.position = "absolute";
+	el.style.left = "-99999px";
+	el.id = "invite_link";
+	el.className = "invite_link";
+	document.body.appendChild(el);
 };
--- conreq/core/base/static/js/generic.js
+++ conreq/core/base/static/js/generic.js
@@ -20,6 +20,11 @@ var add_base_url = function (window_location = null) {
 	return window_location;
 };
 
+// Obtains a URL that can be imported (handles Base URL scenarios)
+var local_js = function (path) {
+	// return STATIC_URL + path
+};
+
 // Gets the current window location from the hash
 var get_window_location = function (raw = false) {
 	// Read the URL hash to determine what page we are on
@@ -102,73 +107,6 @@ var copy_to_clipboard = async function () {
 	// Fallback to legacy copy to clipboard method
 	else {
 		copy_to_clipboard_fallback();
-	}
-};
-
-// Creates a page element that copy_to_clipboard can copy from
-var create_invite_link_elem = function (invite_link) {
-	const el = document.createElement("textarea");
-	el.value = invite_link;
-	el.textContent = invite_link;
-	el.readOnly = true;
-	el.style.position = "absolute";
-	el.style.left = "-99999px";
-	el.id = "invite_link";
-	el.className = "invite_link";
-	document.body.appendChild(el);
-};
-
-// TODO: Remove this
-// Post to a URL
-var post_url = function (url, callback) {
-	http_request.abort();
-	http_request = $.ajax({
-		type: "POST",
-		url: url,
-		headers: {
-			"X-CSRFToken": document.getElementsByName("csrfmiddlewaretoken")[0]
-				.value,
-		},
-		success: callback,
-	});
-	return http_request;
-};
-
-// TODO: Remove this
-// Post JSON to a URL
-var post_json = function (url, data, callback) {
-	http_request.abort();
-	http_request = $.ajax({
-		type: "POST",
-		url: url,
-		headers: {
-			"X-CSRFToken": document.getElementsByName("csrfmiddlewaretoken")[0]
-				.value,
-		},
-		data: JSON.stringify(data),
-		contentType: "application/json; charset=utf-8",
-		success: callback,
-	});
-	return http_request;
-};
-
-// TODO: Remove this
-// Gets a URL
-var get_url = function (location, success = function () {}) {
-	http_request.abort();
-	http_request = $.get(location, function (response = null) {
-		return success(response);
-	});
-	return http_request;
-};
-
-// Hide any floaty objects
-var hide_popups = async function () {
-	$("#modal-container").modal("hide");
-	if (window.matchMedia("(max-width: 800px)").matches) {
-		if (!$("#sidebar").hasClass("collapsed")) {
-			$("#sidebar").addClass("collapsed");
-		}
 	}
 };
 
