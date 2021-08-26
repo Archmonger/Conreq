5759bfa0aecf61a09f2e6ee5e1d965dabd7c1a18
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-08-26 06:28:16 -0700

    organize view and template config vars

--- conreq/app/configuration.py
+++ conreq/app/configuration.py
@@ -16,17 +16,23 @@ class _Config:
     setting_scripts: list = field(default_factory=list)
     installed_apps: list = field(default_factory=list)
     middleware: list = field(default_factory=list)
-    landing_template: str = ""
+
+    # Views
     landing_view: Callable = landing
-    home_template: str = "homepage/home.html"
     home_view: Callable = home
-    sign_up_template: str = "registration/sign_up.html"
     sign_up_view: Callable = sign_up
-    sign_in_template: str = "registration/sign_in.html"
     sign_in_view: Callable = sign_in
-    password_reset_template: str = ""
     password_reset_view: Callable = None
+
+    # Templates
+    landing_template: str = ""
+    home_template: str = "homepage/home.html"
+    sign_up_template: str = "registration/sign_up.html"
+    sign_in_template: str = "registration/sign_in.html"
+    password_reset_template: str = ""
     loading_animation_template: str = "loading/spinner.html"
+
+    # Components
     manage_users_component: Callable = None
     server_settings_component: Callable = None
 
