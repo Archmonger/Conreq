c0e1a9b5efc1e209f773c9155f429544ead8c01e
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-08-26 06:32:02 -0700

    complete app.register.templates

--- conreq/app/register/templates.py
+++ conreq/app/register/templates.py
@@ -1,32 +1,31 @@
-# TODO: Create these functions
-# pylint: disable=unused-argument,unused-variable,unnecessary-pass
+from conreq import app
 
 
 def landing_template(template: str) -> None:
     """Changes the landing page variable."""
-    pass
+    app.config("landing_template", template)
 
 
 def home_template(template: str) -> None:
     """Changes the home page variable."""
-    pass
+    app.config("home_template", template)
 
 
 def sign_up_template(template: str) -> None:
     """Changes the sign up page variable."""
-    pass
+    app.config("sign_up_template", template)
 
 
 def sign_in_template(template: str) -> None:
     """Changes the sign in page variable."""
-    pass
+    app.config("sign_in_template", template)
 
 
 def password_reset_template(template: str) -> None:
     """Changes the password reset page variable."""
-    pass
+    app.config("password_reset_template", template)
 
 
 def loading_animation_template(template: str) -> None:
     """Changes the loading animation template variable."""
-    pass
+    app.config("loading_animation_template", template)
