f9989ee8f8f309b028749db2c0934a095321d59e
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-08-26 21:50:31 -0700

    view function performance metrics

--- conreq/app/register/wsgi.py
+++ conreq/app/register/wsgi.py
@@ -7,9 +7,21 @@ from django.views.generic import View
 from conreq import app
 from conreq.utils.environment import get_base_url
 
+METRICS = None
 BASE_URL = get_base_url(append_slash=False, prepend_slash=False)
 if BASE_URL:
     BASE_URL = BASE_URL + "/"
+
+
+def _load_performance_metrics():
+    # pylint: disable=global-statement,import-outside-toplevel
+
+    global METRICS
+
+    if not METRICS:
+        from conreq.utils.debug import performance_metrics
+
+        METRICS = performance_metrics
 
 
 def url(
@@ -21,17 +33,20 @@ def url(
 
     def decorator(func_or_cls: Union[Callable, View]):
 
-        view = func_or_cls
+        _load_performance_metrics()
+
         if isinstance(func_or_cls, View):
-            view = view.as_view()
+            view = METRICS()(func_or_cls.as_view())
+        else:
+            view = METRICS()(func_or_cls)
 
         url_patterns = app.config("url_patterns")
         if not use_regex:
-            url_patterns.append(urls.path(BASE_URL + path, func_or_cls, name=name))
+            url_patterns.append(urls.path(BASE_URL + path, view, name=name))
         else:
-            url_patterns.append(urls.re_path(BASE_URL + path, func_or_cls, name=name))
+            url_patterns.append(urls.re_path(BASE_URL + path, view, name=name))
 
-        @wraps(func_or_cls)
+        @wraps(view)
         def _wrapped_view(*args, **kwargs):
             return _wrapped_view(*args, **kwargs)
 
@@ -47,19 +62,20 @@ def api(
 
     def decorator(func_or_cls: Union[Callable, View]):
 
-        view = func_or_cls
+        _load_performance_metrics()
+
         if isinstance(func_or_cls, View):
-            view = view.as_view()
+            view = METRICS()(func_or_cls.as_view())
+        else:
+            view = METRICS()(func_or_cls)
 
         url_patterns = app.config("url_patterns")
         if not use_regex:
-            url_patterns.append(urls.path(f"{BASE_URL}/v{version}/{path}", func_or_cls))
+            url_patterns.append(urls.path(f"{BASE_URL}/v{version}/{path}", view))
         else:
-            url_patterns.append(
-                urls.re_path(f"{BASE_URL}/v{version}/{path}", func_or_cls)
-            )
+            url_patterns.append(urls.re_path(f"{BASE_URL}/v{version}/{path}", view))
 
-        @wraps(func_or_cls)
+        @wraps(view)
         def _wrapped_view(*args, **kwargs):
             return _wrapped_view(*args, **kwargs)
 
