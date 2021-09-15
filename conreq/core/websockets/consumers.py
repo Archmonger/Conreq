1c082c372683bec99ea538ef35ea770e3d107b60
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-09-15 00:14:00 -0700

    clean up websocket code

--- conreq/core/websockets/consumers.py
+++ conreq/core/websockets/consumers.py
@@ -2,7 +2,6 @@
 from channels.auth import login
 from channels.db import database_sync_to_async as convert_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
-from django.contrib.auth.models import AnonymousUser
 
 from conreq.utils import log
 
@@ -10,22 +9,29 @@ _logger = log.get_logger(__name__)
 
 
 class BaseWebsocket(AsyncJsonWebsocketConsumer):
-    """Conreq websocket communication."""
+    """
+    Conreq's base websocket. Performs basic tasks such as letting client
+    browsers know if a page refresh is needed.
+    """
 
-    # INITIAL CONNECTION
     async def connect(self):
-        """When the browser attempts to connect to the server."""
-        # Accept the connection
+        """Event that occurs when the browser attempts to connect to the server."""
+        # Accept any connection
         await self.accept()
 
         # Attempt logging in the user
         try:
-            # Log in the user to this session.
-            await login(self.scope, self.scope["user"])
-            # Save the session to the database
-            await convert_to_async(self.scope["session"].save)()
+            if self.scope["user"].is_authenticated:
+                # Log in the user to this session
+                await login(self.scope, self.scope["user"])
+                # Save the session to the database
+                await convert_to_async(self.scope["session"].save)()
+            else:
+                # Signal that the user shouldn't be here
+                await self.__forbidden()
+
+        # User could not be logged in
         except Exception:
-            # User could not be logged in
             log.handler(
                 "Websocket login failure on initial connection!",
                 log.ERROR,
@@ -33,19 +39,19 @@ class BaseWebsocket(AsyncJsonWebsocketConsumer):
             )
             await self.__forbidden()
 
-    # RECEIVING COMMANDS
     async def receive_json(self, content, **kwargs):
-        """When the browser attempts to send a message to the server."""
+        """Event that occurs when the browser sent a message to the server."""
         log.handler(
-            content,
+            f"Message received: {content}",
             log.INFO,
             _logger,
         )
+
         # Reject users that aren't logged in
         if not self.scope["user"].is_authenticated:
             await self.__forbidden()
 
-    # COMMAND RESPONSE: FORBIDDEN
     async def __forbidden(self):
+        """Notify an unauthenticated user that they shouldn't be here."""
         response = {"command_name": "forbidden"}
         await self.send_json(response)
