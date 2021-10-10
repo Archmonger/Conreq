4278007ec8f20495c47c11e5b9db7617e6911a8e
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-10-09 23:59:29 -0700

    more cache cleanup

--- conreq/utils/cache.py
+++ conreq/utils/cache.py
@@ -3,7 +3,7 @@ import logging
 from collections.abc import Callable
 from typing import Any
 
-from django.core.cache import cache
+from django.core.cache import cache as djcache
 from huey.contrib.djhuey import db_task
 
 from conreq.utils.generic import clean_string
@@ -13,19 +13,9 @@ DEFAULT_CACHE_DURATION = 60 * 60  # Time in seconds
 _logger = logging.getLogger(__name__)
 
 
-def create_cache_key(
-    cache_name: str, cache_args: list, cache_kwargs: dict, key: str
-) -> str:
+def create_cache_key(cache_name: str, args: list, kwargs: dict, key: str) -> str:
     """Generates a key to be used with django caching"""
-    return clean_string(
-        cache_name
-        + "_args"
-        + str(cache_args)
-        + "_kwargs"
-        + str(cache_kwargs)
-        + "_key"
-        + str(key)
-    )
+    return clean_string(f"{cache_name}_args_{args}_kwargs_{kwargs}_key_{key}")
 
 
 def obtain_key_from_cache_key(cache_key: str) -> str:
@@ -34,32 +24,35 @@ def obtain_key_from_cache_key(cache_key: str) -> str:
 
 
 @db_task()
-def lazy_set_many(cache_dict, duration):
-    cache.set_many(cache_dict, duration)
+def _lazy_set_many(cache_dict, duration):
+    """Sets a cache value through a background task"""
+    djcache.set_many(cache_dict, duration)
 
 
 @db_task()
-def lazy_set(cache_key, cache_value, duration):
-    cache.set(cache_key, cache_value, duration)
+def _lazy_set(cache_key, cache_value, duration):
+    """Sets many cache values through a background task"""
+    djcache.set(cache_key, cache_value, duration)
 
 
-def handler(
+def get_or_set(
     cache_name: str,
     page_key: str = "",
     function: Callable = None,
-    force_update_cache: bool = False,
-    duration: int = DEFAULT_CACHE_DURATION,
     args: list = (),
     kwargs: dict = None,
+    duration: int = DEFAULT_CACHE_DURATION,
+    force_update: bool = False,
 ) -> Any:
-    """Handles caching for results and data.
+    """Handles caching for results and data. If the cached value is expired and a function is provided,
+    the function is called and a new cached value will be set.
 
     Args:
         cache_name: Name prepended to cache get/set calls.
         page_key: A value to use as a page key.
         function: The function to be executed (if cached values are expired).
             If no function is provided, whatever was stored in cache is always returned.
-        force_update_cache: Forces execution of function, regardless if value is expired or not. Does not work with multi execution.
+        force_update: Forces execution of function, regardless if value is expired or not. Does not work with multi execution.
         duration: Duration in seconds that the cached value should be valid for.
         args: A list of arguements to pass into function.
         kwargs: A dictionary of keyworded arguements to pass into function.
@@ -74,7 +67,7 @@ def handler(
 
         # Get the cached value
         cache_key = create_cache_key(cache_name, args, kwargs, page_key)
-        cached_results = cache.get(cache_key)
+        cached_results = djcache.get(cache_key)
         _logger.debug("%s - Generated cache key %s", cache_name, cache_key)
 
         # No function was provided, just return a bare cache value
@@ -84,10 +77,10 @@ def handler(
 
         # If the user wants to force update the cache, nothing
         # was in cache, or cache was expired, run function()
-        if cached_results is None or force_update_cache:
+        if cached_results is None or force_update:
             function_results = function(*args, **kwargs)
             if function_results:
-                lazy_set(cache_key, function_results, duration)
+                _lazy_set(cache_key, function_results, duration)
             _logger.info("%s - %s()", cache_name, function.__name__)
             return function_results
 
@@ -99,19 +92,20 @@ def handler(
 
     except Exception:
         # If the function threw an exception, return none.
+        failed_func = function
         if hasattr(function, "__name__"):
-            _logger.exception("Function %s failed to execute!", function.__name__)
+            failed_func = function.__name__
         else:
             _logger.exception(
-                "Cache handler has failed! Function: %s Cache Name: %s Page Key: %s",
-                function,
+                "Cache handler has failed to execute. Function: %s Cache Name: %s Page Key: %s",
+                failed_func,
                 cache_name,
                 page_key,
             )
     return None
 
 
-def multi_handler(
+def get_or_set_many(
     cache_name: str,
     functions: dict[dict],
     duration: int = DEFAULT_CACHE_DURATION,
@@ -145,7 +139,7 @@ def multi_handler(
             requested_keys.append(cache_key)
 
         # Search cache for all keys
-        cached_results = cache.get_many(requested_keys)
+        cached_results = djcache.get_many(requested_keys)
         _logger.info(
             "%s - Cache multi execution detected %d available keys.",
             cache_name,
@@ -176,7 +170,7 @@ def multi_handler(
                 cache_name,
                 missing_keys,
             )
-            lazy_set_many(missing_keys, duration)
+            _lazy_set_many(missing_keys, duration)
 
         # Return all results
         cached_results.update(missing_keys)
