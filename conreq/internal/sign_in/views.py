604d0fe9ac0844fdb064be42b5fcd35a20a0f69f
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-10-27 21:13:33 -0700

    fix precommit error

--- conreq/internal/sign_in/views.py
+++ conreq/internal/sign_in/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.views import LoginView
 
-from conreq.app import register
+from conreq.app import config, register
 
 
 @register.sign_in_view()
 def sign_in(request):
-    return LoginView.as_view(template_name=app.config.sign_in_template)(request)
+    return LoginView.as_view(template_name=config.sign_in_template)(request)
