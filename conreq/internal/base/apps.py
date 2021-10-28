d9af89c55b9bc2eaff4b4a189db3954ae0b9f7e9
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-10-28 01:37:22 -0700

    register base websocket

--- conreq/internal/base/apps.py
+++ conreq/internal/base/apps.py
@@ -4,6 +4,7 @@ from django_idom.websocket_consumer import IdomAsyncWebsocketConsumer
 from idom.html import i
 
 from conreq.app import register
+from conreq.internal.websockets.consumers import BaseWebsocket
 
 
 class BaseConfig(AppConfig):
@@ -13,10 +14,12 @@ class BaseConfig(AppConfig):
         # pylint: disable=import-outside-toplevel
         from django_idom.config import IDOM_WEB_MODULES_URL, IDOM_WEBSOCKET_URL
 
+        register.websocket("")(BaseWebsocket)
         register.websocket(IDOM_WEBSOCKET_URL + "<view_id>/")(
             IdomAsyncWebsocketConsumer
         )
         register.url(IDOM_WEB_MODULES_URL + "<path:file>")(web_modules_file)
+
         # TODO: Remove this later
         register.nav_group("User", i({"className": "fas fa-users icon-left"}))
         register.nav_tab("User", "Settings")(lambda X: None)
