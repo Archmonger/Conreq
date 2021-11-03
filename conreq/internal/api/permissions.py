f992721fadcd0d4bcca2c98d65251c7f98f14a38
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-11-02 23:34:56 -0700

    better get_key

--- conreq/internal/api/permissions.py
+++ conreq/internal/api/permissions.py
@@ -10,9 +10,5 @@ class HasAPIKey(BaseHasAPIKey):
     key_parser = KeyParser()
 
     def get_key(self, request: HttpRequest) -> typing.Optional[str]:
-        # Prefer key in header
-        header_key = self.key_parser.get(request)
-        if header_key:
-            return header_key
-        # Fallback to key in URL parameters
-        return request.GET.get("apikey")
+        # Prefer key in header, fallback to URL parameters.
+        return self.key_parser.get(request) or request.GET.get("apikey")
