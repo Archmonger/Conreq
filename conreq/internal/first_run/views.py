8504a41793c53f911fba35f42790bd50302560b7
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-11-16 04:34:15 -0800

    set default COMPRESS_RESPONSES on init

--- conreq/internal/first_run/views.py
+++ conreq/internal/first_run/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect
 from django.template import loader
 
 from conreq.internal.server_settings.models import ServerConfig
+from conreq.utils.environment import get_env, set_env
 
 from .forms import InitializationForm
 
@@ -22,7 +23,7 @@ def initialize(request):
 
         # Create the superuser and set up the database if the form is valid
         if form.is_valid():
-            return _first_time_configuration(form, request, server_config)
+            return _first_run_setup(form, request, server_config)
 
         # Form data wasn't valid, so return the error codes
         template = loader.get_template("registration/initialization.html")
@@ -33,7 +34,7 @@ def initialize(request):
     return HttpResponse(template.render({}, request))
 
 
-def _first_time_configuration(form, request, server_config):
+def _first_run_setup(form, request, server_config):
     form.save()
     username = form.cleaned_data.get("username")
     password = form.cleaned_data.get("password1")
@@ -45,4 +46,6 @@ def _first_time_configuration(form, request, server_config):
     login(request, user)
     server_config.initialized = True
     server_config.save()
+    if get_env("COMPRESS_RESPONSES", return_type=bool) is None:
+        set_env("COMPRESS_RESPONSES", "true")
     return redirect("landing:main")
