1b972eeafc4eda2cbe8521c5044270bcbd9f9020
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-11-16 05:42:21 -0800

    AutoOneToOneField

--- conreq/internal/manage_users/models.py
+++ conreq/internal/manage_users/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from model_utils import FieldTracker
+
+from conreq.utils.fields import AutoOneToOneField
 
 User = get_user_model()
 
@@ -11,21 +11,7 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = AutoOneToOneField(User, on_delete=models.CASCADE)
     language = models.CharField(max_length=30, default="en-US")
     externally_authenticated = models.BooleanField(default=False)
     tracker = FieldTracker()
-
-
-# TODO: Make a generic version of this in the database utils
-# Perhaps call it auto_generate_model?
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    # pylint: disable=unused-argument
-    # Create the profile if it doesn't exist
-    if not hasattr(instance, "profile"):
-        Profile.objects.create(user=instance)
-
-    # Save the profile if it has changed
-    if instance.profile.tracker.changed():
-        instance.profile.save()
--- conreq/utils/fields.py
+++ conreq/utils/fields.py
@@ -1,4 +1,6 @@
-from django.db.models import URLField
+from django.db.models import OneToOneField, URLField
+from django.db.models.fields.related_descriptors import ReverseOneToOneDescriptor
+from django.db.transaction import atomic
 
 from . import forms, validators
 
@@ -9,11 +11,53 @@ class HostnameOrURLField(URLField):
     default_validators = [validators.ExtendedURLValidator()]
 
     def formfield(self, **kwargs):
-        # As with CharField, this will cause URL validation to be performed
-        # twice.
+        # As with CharField, this will cause URL validation to be performed twice.
         return super().formfield(
             **{
                 "form_class": forms.HostnameOrURLField,
                 **kwargs,
             }
         )
+
+
+class AutoOneToOneField(OneToOneField):
+    """
+    OneToOneField creates related object on first call if it doesnt exist yet.
+    Use it instead of original OneToOne field.
+    example:
+        class MyProfile(models.Model):
+            user = AutoOneToOneField(User, primary_key=True)
+            home_page = models.URLField(max_length=255, blank=True)
+            icq = models.IntegerField(max_length=255, null=True)
+    """
+
+    def contribute_to_related_class(self, cls, related):
+        setattr(
+            cls,
+            related.get_accessor_name(),
+            _AutoSingleRelatedObjectDescriptor(related),
+        )
+
+
+class _AutoSingleRelatedObjectDescriptor(ReverseOneToOneDescriptor):
+    """
+    The descriptor that handles the object creation for an AutoOneToOneField.
+    """
+
+    @atomic
+    def __get__(self, instance, instance_type=None):
+        model = getattr(self.related, "related_model", self.related.model)
+
+        try:
+            return super(_AutoSingleRelatedObjectDescriptor, self).__get__(
+                instance, instance_type
+            )
+        except model.DoesNotExist:
+            # Using get_or_create instead() of save() or create() as it better handles race conditions
+            obj, _ = model.objects.get_or_create(**{self.related.field.name: instance})
+
+            # Update Django's cache, otherwise first 2 calls to obj.relobj
+            # will return 2 different in-memory objects
+            self.related.set_cached_value(instance, obj)
+            self.related.field.set_cached_value(obj, instance)
+            return obj
