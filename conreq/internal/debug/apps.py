97c19949dec8f84af9b19b96fd604679f669f3b6
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-11-17 00:45:10 -0800

    create debug app

---
+++ conreq/internal/debug/apps.py
@@ -0,0 +1,79 @@
+from django.apps import AppConfig
+from django.contrib import admin
+from django.urls import include
+
+from conreq.app import register
+from conreq.utils.environment import get_debug
+
+DEBUG = get_debug()
+
+
+class DebugConfig(AppConfig):
+    name = "conreq.internal.debug"
+
+    def ready(self) -> None:
+        if not DEBUG:
+            return
+
+        performance_profiling()
+        admin_panel()
+        password_reset()
+        api_docs()
+
+
+def performance_profiling():
+    register.url("silk/")(include("silk.urls", namespace="silk"))
+
+
+def admin_panel():
+    register.url("admin/docs/")(include("django.contrib.admindocs.urls"))
+    register.url("admin/")(admin.site.urls)
+
+
+def password_reset():
+    from django.contrib.auth import views as auth_views
+
+    register.url("password_reset/", name="password_reset_done")(
+        auth_views.PasswordResetView
+    )
+    register.url("password_reset/done/", name="password_reset_done")(
+        auth_views.PasswordResetDoneView
+    )
+    register.url("reset/<uidb64>/<token>/", name="password_reset_confirm")(
+        auth_views.PasswordResetConfirmView
+    )
+    register.url("reset/done/", name="password_reset_complete")(
+        auth_views.PasswordResetCompleteView
+    )
+
+
+def api_docs():
+    from drf_yasg import openapi
+    from drf_yasg.views import get_schema_view
+    from rest_framework import permissions
+
+    # Django Rest Framework documentation (Swagger and Redoc)
+    SchemaView = get_schema_view(
+        openapi.Info(
+            title="Conreq API Endpoints",
+            default_version="v1",
+            description="""
+            This page contains a lost of all endpoints available within this Conreq instance.
+
+            Endpoints require an API key either in **HTTP Header (Authorization: Api-Key)** or in the **URL Parameter (apikey)**.
+
+            Token Authentication is performed using **HTTP Header (Authorization: Token)**. Session Authentication can alternatively be performed.
+            """,
+            contact=openapi.Contact(email="archiethemonger@gmail.com"),
+            license=openapi.License(name="GPL-3.0 License"),
+        ),
+        public=True,
+        permission_classes=[permissions.AllowAny],
+    )
+
+    register.url(
+        r"^swagger(?P<format>\.json|\.yaml)$", name="schema-json", use_regex=True
+    )(SchemaView.without_ui(cache_timeout=0))
+    register.url(r"^swagger/$", name="schema-swagger-ui", use_regex=True)(
+        SchemaView.with_ui("swagger", cache_timeout=0)
+    )
--- conreq/urls.py
+++ conreq/urls.py
@@ -1,7 +1,7 @@
 """Conreq URL Configuration"""
 
+
 from django.conf import settings
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 from django.views.generic.base import RedirectView
@@ -19,93 +19,11 @@ urlpatterns = [
     path("", include("conreq.internal.landing.urls")),
     path("", include("conreq.internal.home.urls")),
     path("", include("conreq.internal.pwa.urls")),
-    path(
-        "sign_in/",
-        app.config.sign_in_view,
-        name="sign_in",
-    ),
+    path("sign_in/", app.config.sign_in_view, name="sign_in"),
     path("sign_out/", auth_views.logout_then_login, name="sign_out"),
     path("sign_up/", include("conreq.internal.sign_up.urls")),
-    path("manage_users/", include("conreq.internal.manage_users.urls")),
-    path("server_settings/", include("conreq.internal.server_settings.urls")),
     path("api/v1/", include("conreq.internal.api.urls")),
 ]
-
-# Debug tools
-if DEBUG:
-    # Performance analysis tool
-    urlpatterns.append(path("silk/", include("silk.urls", namespace="silk")))
-    # Ability to edit the DB from admin/
-    urlpatterns.append(path("admin/docs/", include("django.contrib.admindocs.urls")))
-    urlpatterns.append(path("admin/", admin.site.urls))
-    urlpatterns.append(
-        path(
-            "password_reset/",
-            auth_views.PasswordResetView.as_view(),
-            name="password_reset_done",
-        )
-    )
-    urlpatterns.append(
-        path(
-            "password_reset/done/",
-            auth_views.PasswordResetDoneView.as_view(),
-            name="password_reset_done",
-        )
-    )
-    urlpatterns.append(
-        path(
-            "reset/<uidb64>/<token>/",
-            auth_views.PasswordResetConfirmView.as_view(),
-            name="password_reset_confirm",
-        )
-    )
-    urlpatterns.append(
-        path(
-            "reset/done/",
-            auth_views.PasswordResetCompleteView.as_view(),
-            name="password_reset_complete",
-        )
-    )
-    # Django Rest Framework documentation (Swagger and Redoc)
-    # pylint: disable=ungrouped-imports
-    from django.urls import re_path
-    from drf_yasg import openapi
-    from drf_yasg.views import get_schema_view
-    from rest_framework import permissions
-
-    SchemaView = get_schema_view(
-        openapi.Info(
-            title="Conreq API Endpoints",
-            default_version="v1",
-            description="""
-            Outline for all endpoints available within this Conreq instance.
-
-            All endpoints require an API key either in **HTTP Header (Authorization: Api-Key)** or in the **URL Parameter (apikey)**.
-
-            Token Authentication is performed using **HTTP Header (Authorization: Token)**. Session Authentication can alternatively be performed.
-            """,
-            contact=openapi.Contact(email="archiethemonger@gmail.com"),
-            license=openapi.License(name="GPL-3.0 License"),
-        ),
-        public=True,
-        permission_classes=[permissions.AllowAny],
-    )
-
-    docs_urlpatterns = [
-        re_path(
-            r"^swagger(?P<format>\.json|\.yaml)$",
-            SchemaView.without_ui(cache_timeout=0),
-            name="schema-json",
-        ),
-        re_path(
-            r"^swagger/$",
-            SchemaView.with_ui("swagger", cache_timeout=0),
-            name="schema-swagger-ui",
-        ),
-    ]
-
-    for pattern in docs_urlpatterns:
-        urlpatterns.append(pattern)
 
 
 # Wrap the urlpatterns in BASE_URL if required
