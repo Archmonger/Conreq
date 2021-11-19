488244aeaeb890b6f39e935bc8f2ab1754131b98
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-11-19 00:46:31 -0800

    clean up base apps

--- conreq/internal/base/apps.py
+++ conreq/internal/base/apps.py
@@ -15,24 +15,28 @@ class BaseConfig(AppConfig):
 
         conreq.config.websockets.append(IDOM_WEBSOCKET_PATH)
 
-        # TODO: Remove this later
-        # pylint: disable=unused-argument
-        register.nav_group("User", i({"className": "fas fa-users icon-left"}))
 
-        @register.nav_tab("User", "Settings")
-        def settings(websocket, state, set_state):
-            return p("settings")
+# TODO: Remove this later
+# pylint: disable=unused-argument
+register.nav_group("User", i({"className": "fas fa-users icon-left"}))
+register.nav_group("Admin", i({"className": "fas fa-cogs icon-left"}))
 
-        @register.nav_tab("User", "Sign Out", viewport=Viewport.secondary)
-        def sign_out(websocket, state, set_state):
-            return p("sign out")
 
-        register.nav_group("Admin", i({"className": "fas fa-cogs icon-left"}))
+@register.nav_tab("User", "Settings")
+def settings(websocket, state, set_state):
+    return p("settings")
 
-        @register.nav_tab("Admin", "Manage Users")
-        def manage_users(websocket, state, set_state):
-            return p("manage users")
 
-        @register.nav_tab("Admin", "Server Config")
-        def server_config(websocket, state, set_state):
-            return p("server config")
+@register.nav_tab("User", "Sign Out", viewport=Viewport.secondary)
+def sign_out(websocket, state, set_state):
+    return p("sign out")
+
+
+@register.nav_tab("Admin", "Manage Users")
+def manage_users(websocket, state, set_state):
+    return p("manage users")
+
+
+@register.nav_tab("Admin", "Server Config")
+def server_config(websocket, state, set_state):
+    return p("server config")
