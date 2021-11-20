fcf3a9b697f0a366b0c94f92bf632b0206f650f1
Archmonger <16909269+Archmonger@users.noreply.github.com>
2021-11-20 06:36:14 -0800

    api stub returns error

--- conreq/utils/api.py
+++ conreq/utils/api.py
@@ -5,4 +5,4 @@ from rest_framework.response import Response
 @api_view(["GET"])
 def stub(request, *args, **kwargs):
     """This is a stub for an endpoint that has not yet been developed."""
-    return Response({})
+    return Response({"error": "This endpoint is not yet developed."})
