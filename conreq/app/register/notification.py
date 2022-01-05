c3e23a1e51bc556f47879a9fc89d114c45fa4b64
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-01-05 06:35:47 -0800

    add notification agent todo

---
+++ conreq/app/register/notification.py
@@ -0,0 +1,8 @@
+# TODO: Create generic user notification agents
+# Should be a class with some pre-designed endpoints for sending
+# Probaby something like send(user:User, message) and mass_send(user:list[User], message)
+# Need some other generic construct for non-user based notification agents
+
+
+def agent():
+    pass
