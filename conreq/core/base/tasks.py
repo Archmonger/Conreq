2859468cb2d8f7cf15e714ceb0b5124042344008
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-01-04 20:02:39 -0800

    add optimize to vacuum task

--- conreq/core/base/tasks.py
+++ conreq/core/base/tasks.py
@@ -12,17 +12,18 @@ DB_ENGINE = get_database_type()
 HUEY_FILENAME = getattr(settings, "HUEY_FILENAME")
 
 
-@db_periodic_task(crontab(minute="0", hour="0", strict=True))
+@db_periodic_task(crontab(minute="0", hour="1", strict=True))
 def vacuum_huey_sqlite_db():
     """Periodically preforms a SQLITE vacuum on the background task database."""
-    if os.path.exists(HUEY_FILENAME):
-        with sqlite3.connect(HUEY_FILENAME) as cursor:
-            cursor.execute("VACUUM")
+    with sqlite3.connect(HUEY_FILENAME) as cursor:
+        cursor.execute("VACUUM")
+        cursor.execute("PRAGMA optimize")
 
 
-@db_periodic_task(crontab(minute="0", hour="0", strict=True))
+@db_periodic_task(crontab(minute="0", hour="1", strict=True))
 def vacuum_conreq_sqlite_db():
     """Periodically performs any cleanup tasks needed for the default database."""
     if DB_ENGINE == "SQLITE3":
         with connection.cursor() as cursor:
             cursor.execute("VACUUM")
+            cursor.execute("PRAGMA optimize")
