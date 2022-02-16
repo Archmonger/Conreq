d336beb1be559c1b4dc8137080fb687695de7330
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-02-15 16:38:25 -0800

    add skeleton for click events

--- conreq/_core/app_store/components.py
+++ conreq/_core/app_store/components.py
@@ -25,10 +25,11 @@ class PlaceholderApp:
 @component
 def app_store(websocket, state, set_state):
     # pylint: disable=unused-argument
+    tab, set_tab = hooks.use_state(None)
     categories, set_categories = hooks.use_state({})
 
     @hooks.use_effect
-    async def _get_categories():
+    async def load_from_db():
         if categories:
             return
         print("Categories state is empty. Refreshing...")
@@ -40,17 +41,20 @@ def app_store(websocket, state, set_state):
     # TODO: Remove this top level div later https://github.com/idom-team/idom/issues/538
     return div(
         link({"rel": "stylesheet", "href": static("conreq/app_store.css")}),
-        div(
+        tab(key=tab.__name__)
+        if tab
+        else div(
             {"className": "spotlight-region"},
-            recently_added(),
-            recently_updated(),
-            most_popular(),
-            top_downloaded(),
-            our_favorites(),
-            essentials(),
-            random_selection(),
+            recently_added(set_tab),
+            recently_updated(set_tab),
+            most_popular(set_tab),
+            top_downloaded(set_tab),
+            our_favorites(set_tab),
+            essentials(set_tab),
+            random_selection(set_tab),
+            key="spotlight-region",
         ),
-        div({"className": "nav-region"}, nav_constructor(categories)),
+        div({"className": "nav-region"}, nav_constructor(categories, set_tab)),
     )
 
 
@@ -63,11 +67,17 @@ def get_categories() -> dict[Category, list[Subcategory]]:
     return categories
 
 
-def nav_constructor(categories: dict[Category, list[Subcategory]]) -> list:
+def nav_constructor(categories: dict[Category, list[Subcategory]], set_tab) -> list:
     return [
         div(
             {"className": "nav-item"},
-            a({"href": "#"}, h5({"className": "nav-title"}, category.name)),
+            a(
+                {
+                    "href": f"#{category.uuid}",
+                    "onClick": lambda x: print("clicked"),
+                },
+                h5({"className": "nav-title"}, category.name),
+            ),
             ol(
                 {"className": "nav-sub"},
                 [
@@ -76,7 +86,8 @@ def nav_constructor(categories: dict[Category, list[Subcategory]]) -> list:
                         a(
                             {
                                 "className": "nav-sub-link",
-                                "href": f"#/app_store/category/{subcategory.uuid}",
+                                "href": f"#{subcategory.uuid}",
+                                "onClick": lambda x: print("clicked"),
                             },
                             subcategory.name,
                         ),
@@ -91,66 +102,89 @@ def nav_constructor(categories: dict[Category, list[Subcategory]]) -> list:
     ]
 
 
-def recently_added():
-    return spotlight("Recently Added", "The latest from our community.")
+def recently_added(set_tab):
+    return spotlight("Recently Added", "The latest from our community.", set_tab)
 
 
-def recently_updated():
-    return spotlight("Recently Updated", "Actively maintained for all to enjoy.")
+def recently_updated(set_tab):
+    return spotlight(
+        "Recently Updated", "Actively maintained for all to enjoy.", set_tab
+    )
 
 
-def most_popular():
-    return spotlight("Most Popular", "These have gained a serious amount lot of love.")
+def most_popular(set_tab):
+    return spotlight(
+        "Most Popular", "These have gained a serious amount lot of love.", set_tab
+    )
 
 
-def top_downloaded():
-    return spotlight("Top Downloaded", "Tons of downloads!")
+def top_downloaded(set_tab):
+    return spotlight("Top Downloaded", "Tons of downloads!", set_tab)
 
 
-def our_favorites():
-    return spotlight("Our Favorites", "A curated list of our favorites.")
+def our_favorites(set_tab):
+    return spotlight("Our Favorites", "A curated list of our favorites.", set_tab)
 
 
-def essentials():
-    return spotlight("Essentials", "Something we think all users should consider.")
+def essentials(set_tab):
+    return spotlight(
+        "Essentials", "Something we think all users should consider.", set_tab
+    )
 
 
-def random_selection():
-    return spotlight("Random Selection", "Are you feeling lucky today?")
+def random_selection(set_tab):
+    return spotlight("Random Selection", "Are you feeling lucky today?", set_tab)
 
 
-def spotlight(title, description, apps=None):
+def spotlight(title, description, set_tab, apps=None):
     return div(
         {"className": "spotlight"},
         a(
-            {"href": "#"},
+            {"href": "#", "onClick": lambda x: print("clicked")},
             h4({"className": "title"}, title),
             p({"className": "description"}, description),
         ),
         div(
             {"className": "card-stage"},
-            [card() for _ in range(8)],
+            [card(set_tab) for _ in range(8)],
         ),
     )
 
 
-def card(app: PlaceholderApp = PlaceholderApp()):
+def card(set_tab, app: PlaceholderApp = PlaceholderApp()):
     return div(
         {"className": "card"},
         div(
             {"className": "top"},
             div(
                 {"className": "text-region"},
-                h5({"className": "card-title"}, app.name),
-                div({"className": "card-author"}, app.author),
-                div({"className": "card-category"}, app.category),
+                h5(
+                    {"className": "card-title"},
+                    a(
+                        {"href": f"#{app.id}", "onClick": lambda x: print("clicked")},
+                        app.name,
+                    ),
+                ),
+                div(
+                    {"className": "card-author"},
+                    a(
+                        {"href": "#", "onClick": lambda x: print("clicked")},
+                        app.author,
+                    ),
+                ),
+                div(
+                    {"className": "card-category"},
+                    a(
+                        {"href": "#", "onClick": lambda x: print("clicked")},
+                        app.category,
+                    ),
+                ),
             ),
             div({"className": "image"}),
         ),
         div(
             {"className": "btn-container"},
-            div({"className": "btn btn-sm btn-secondary"}, "Info"),
-            div({"className": "btn btn-sm btn-secondary"}, "Repo"),
+            div({"className": "btn btn-sm btn-secondary"}, "Details"),
             div({"className": "btn btn-sm btn-primary"}, "Install"),
         ),
         div({"className": "version"}, f"Version: {app.version}"),
