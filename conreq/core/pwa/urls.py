e5ae3833e17129ce77f26cf3138c1d49a62f6fed
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-04-13 13:20:11 -0700

    fix service worker

--- conreq/core/pwa/urls.py
+++ conreq/core/pwa/urls.py
@@ -1,11 +1,11 @@
-from django.urls import re_path
+from django.urls import path
 
 from .views import manifest, offline, service_worker
 
 app_name = "pwa"
 # Serve up serviceworker.js and manifest.json at the root
 urlpatterns = [
-    re_path(r"^serviceworker\.js$", service_worker, name="serviceworker"),
-    re_path(r"^manifest\.json$", manifest, name="manifest"),
-    re_path(r"^offline/$", offline, name="offline"),
+    path("serviceworker.js", service_worker, name="serviceworker"),
+    path("manifest.json", manifest, name="manifest"),
+    path("offline/", offline, name="offline"),
 ]
