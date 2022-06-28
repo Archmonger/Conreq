e14e12cd056cd886285137e61263c9e19dccf12e
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-06-28 03:07:26 -0700

    minor server settings form tweaks

--- conreq/_core/server_settings/forms.py
+++ conreq/_core/server_settings/forms.py
@@ -24,20 +24,27 @@ from conreq.utils.environment import get_home_url
 
 
 class GeneralSettingsForm(EnvFormMixin, ModelForm):
-    session_max_age = EnvIntegerField(
-        env_name="SESSION_COOKIE_AGE",
-        initial=settings.SESSION_COOKIE_AGE,
-        help_text="Maximum time in seconds for a user session to last. <b>Restart for this change to take effect.</b>",
+    base_url = EnvCharField(
+        env_name="BASE_URL",
+        label="Base URL",
+        help_text='Appears in all links. A value of "base-url" will result in example.com/base-url.',
+    )
+    home_url = EnvCharField(
+        env_name="HOME_URL",
+        label="Home URL",
+        initial=get_home_url(False, False),
+        required=True,
+        help_text='Appears in homepage links. A value of "home-url" will result in example.com/base-url/home-url.',
     )
     debug_mode = EnvBooleanField(
         env_name="DEBUG_MODE",
         initial=settings.DEBUG,
-        help_text="Disables security features and adds debugging tools. <b>Restart for this change to take effect.</b>",
+        help_text="Disables security features and adds debugging tools.",
     )
     safe_mode = EnvBooleanField(
         env_name="SAFE_MODE",
         initial=settings.SAFE_MODE,
-        help_text="Disables all installed apps. <b>Restart for this change to take effect.</b>",
+        help_text="Disables all installed apps.",
     )
     conreq_version = CharField(
         initial=settings.CONREQ_VERSION, disabled=True, required=False
@@ -46,7 +53,14 @@ class GeneralSettingsForm(EnvFormMixin, ModelForm):
 
     class Meta:
         model = GeneralSettings
-        fields = ("server_name", "server_description", "public_url", "app_store_url")
+        fields = (
+            "server_name",
+            "server_description",
+            "base_url",
+            "home_url",
+            "public_url",
+            "app_store_url",
+        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -93,26 +107,23 @@ class ReferrerPolicy(TextChoices):
 # TODO: Set these initial values based on settings.py
 class WebserverSettingsForm(EnvFormMixin, ModelForm):
     # Remember to change the default in settings.py if you change these defaults
-    base_url = EnvCharField(
-        env_name="BASE_URL",
-        label="Base URL",
-        help_text='Appears in all links. A value of "base-url" will result in example.com/base-url.',
-    )
-    home_url = EnvCharField(
-        env_name="HOME_URL",
-        label="Home URL",
-        initial=get_home_url(False, False),
-        help_text='Appears in homepage links. A value of "home-url" will result in example.com/base-url/home-url.',
+    session_max_age = EnvIntegerField(
+        env_name="SESSION_COOKIE_AGE",
+        initial=settings.SESSION_COOKIE_AGE,
+        required=True,
+        help_text="Maximum time in seconds for a user session to last.",
     )
     secure_referrer_policy = EnvChoiceField(
         env_name="SECURE_REFERRER_POLICY",
         initial=ReferrerPolicy.STRICT_ORIGIN_WHEN_CROSS_ORIGIN,
         choices=ReferrerPolicy.choices,
+        required=True,
         help_text="The HTTP referrer policy to apply.",
     )
     allowed_domains = EnvCharField(
         env_name="ALLOWED_HOSTS",
         initial=",".join(settings.ALLOWED_HOSTS),
+        required=True,
         help_text="Comma separated list of IPs or domains the webserver is allowed to \
             serve (ex. example.com, 127.0.0.1). Wildcards (*) are allowed within the URLs.",
     )
@@ -133,17 +144,20 @@ class WebserverSettingsForm(EnvFormMixin, ModelForm):
         env_name="WEBSERVER_HOST",
         label="Host IP",
         initial="0.0.0.0",
+        required=True,
         help_text="Networking address to bind to. For all hosts, use 0.0.0.0.",
     )
     host_port = EnvIntegerField(
         env_name="WEBSERVER_PORT",
         initial=7575,
         max_value=65535,
+        required=True,
         help_text="Port number to bind to.",
     )
     worker_processes = EnvIntegerField(
         env_name="WEBSERVER_WORKERS",
         initial=settings.WEBSERVER_WORKERS,
+        required=True,
         help_text="Number of separate worker processes for the webserver to use. <b>Each worker uses additional memory.</b>",
     )
     webserver_debug = EnvBooleanField(
@@ -154,14 +168,13 @@ class WebserverSettingsForm(EnvFormMixin, ModelForm):
     class Meta:
         model = WebserverSettings
         fields = [
-            "base_url",
-            "home_url",
+            "host_ip",
+            "host_port",
+            "session_max_age",
+            "rotate_secret_key",
             "secure_referrer_policy",
             "allowed_domains",
             "csrf_trusted_origins",
-            "rotate_secret_key",
-            "host_ip",
-            "host_port",
             "worker_processes",
             "ssl_ca_certificate",
             "ssl_certificate",
