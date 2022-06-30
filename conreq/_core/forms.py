e9d7cd0b120cd376cdc4ed1a183cf51513af35bd
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-06-30 05:44:25 -0700

    don't save to settings.env unless value has changed

--- conreq/_core/forms.py
+++ conreq/_core/forms.py
@@ -1,7 +1,7 @@
 from django.forms import BooleanField, CharField, ChoiceField, IntegerField, URLField
 
 from conreq._core import validators, widgets
-from conreq.utils.environment import set_env
+from conreq.utils.environment import get_env, set_env
 from conreq.utils.forms import EnvFieldMixin
 
 
@@ -33,19 +33,43 @@ class URLOrRelativeURLField(URLField):
 class EnvCharField(EnvFieldMixin, CharField):
     """A character field that utilizes the env file, instead of the database."""
 
+    def __init__(self, env_name, *, required=False, **kwargs) -> None:
+        super().__init__(env_name, required=required, **kwargs)
+        if self.initial is None:
+            self.initial = get_env(self.env_name, default_value="")
+
 
 class EnvChoiceField(EnvFieldMixin, ChoiceField):
     """A choice field that utilizes the env file, instead of the database."""
 
+    def __init__(self, env_name, *, required=False, **kwargs) -> None:
+        super().__init__(env_name, required=required, **kwargs)
+        if self.initial is None:
+            raise ValueError(
+                f"`initial=...` parameter is required for EnvChoiceField {self.env_name}"
+            )
+
 
 class EnvBooleanField(EnvFieldMixin, BooleanField):
     """A boolean field that utilizes the env file, instead of the database."""
+
+    def __init__(self, env_name, *, required=False, **kwargs) -> None:
+        super().__init__(env_name, required=required, **kwargs)
+        if self.initial is None:
+            self.initial = get_env(self.env_name, default_value=False, return_type=bool)
 
     env_type = bool
 
 
 class EnvIntegerField(EnvFieldMixin, IntegerField):
     """An integer field that utilizes the env file, instead of the database."""
+
+    def __init__(self, env_name, *, required=False, **kwargs) -> None:
+        super().__init__(env_name, required=required, **kwargs)
+        if self.initial is None:
+            raise ValueError(
+                f"`initial=...` parameter is required for EnvIntegerField {self.env_name}"
+            )
 
     env_type = int
 
@@ -67,7 +91,15 @@ class EnvFormMixin:
             return saved
 
         for name, field in self.base_fields.items():
-            if isinstance(field, EnvFieldMixin):
+            if isinstance(field, EnvFieldMixin) and self._env_changed(name, field):
                 set_env(field.env_name or name, self.cleaned_data.get(name))
 
         return saved
+
+    def _env_changed(self, name, field):
+        """Check if a new value was provided that is different from either the
+        initial or stored value."""
+        initial_value = str(field.initial)
+        current_value = get_env(field.env_name, default_value=initial_value)
+        new_value = str(self.cleaned_data.get(name, ""))
+        return current_value != new_value
