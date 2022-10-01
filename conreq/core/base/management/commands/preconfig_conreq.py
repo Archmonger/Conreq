da001d7331f4d3ffdcff07960a43868a8a2e0c69
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-10-01 05:52:08 -0700

    better ordering of permission logic

--- conreq/core/base/management/commands/preconfig_conreq.py
+++ conreq/core/base/management/commands/preconfig_conreq.py
@@ -101,17 +101,20 @@ class Command(BaseCommand):
             for dirpath, dirnames, filenames in os.walk(path):
                 try:
                     shutil.chown(dirpath, new_uid, new_gid)
-                    for filename in filenames:
-                        shutil.chown(os.path.join(dirpath, filename), new_uid, new_gid)
+                    try:
+                        for filename in filenames:
+                            shutil.chown(
+                                os.path.join(dirpath, filename), new_uid, new_gid
+                            )
+                    except FileNotFoundError:
+                        print(
+                            'Unable to apply permissions to "'
+                            + os.path.join(dirpath, filename)
+                            + '". File not found.'
+                        )
                 except PermissionError:
                     print(
                         'Unable to apply permissions to "'
                         + dirpath
                         + '". Permission denied.'
-                    )
-                except FileNotFoundError:
-                    print(
-                        'Unable to apply permissions to "'
-                        + dirpath
-                        + '". File not found.'
                     )
