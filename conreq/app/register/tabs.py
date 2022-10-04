b176e44a41d415bc24269e5a10fbef79e0e661fd
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-10-04 16:04:11 -0700

    flesh out tabs API

--- conreq/app/register/tabs.py
+++ conreq/app/register/tabs.py
@@ -1,44 +1,32 @@
-from typing import Callable
-
 from conreq import config
 from conreq.types import SubTab
 
 
-def user_settings(name: str) -> Callable:
+def user_settings(name: str):
     """Decorates an IDOM component. Tabs are added to the user settings page."""
 
     def decorator(func):
-        config.tabs.user_settings.append(SubTab(name=name, component=func))
+        config.tabs.user_settings.installed.append(SubTab(name=name, component=func))
         return func
 
     return decorator
 
 
-def manage_users(name: str) -> Callable:
+def manage_users(name: str):
     """Decorates an IDOM component. Tabs are added to the manage users page."""
 
     def decorator(func):
-        config.tabs.user_management.append(SubTab(name=name, component=func))
+        config.tabs.user_management.installed.append(SubTab(name=name, component=func))
         return func
 
     return decorator
 
 
-def app_store(name: str) -> Callable:
-    """Decorates an IDOM component. Tabs are added to the app store page."""
-
-    def decorator(func):
-        config.tabs.app_store.append(SubTab(name=name, component=func))
-        return func
-
-    return decorator
-
-
-def server_settings(name: str) -> Callable:
+def server_settings(name: str):
     """Decorates an IDOM component. Tabs are added to the user server settings page."""
 
     def decorator(func):
-        config.tabs.server_settings.append(SubTab(name=name, component=func))
+        config.tabs.server_settings.installed.append(SubTab(name=name, component=func))
         return func
 
     return decorator
