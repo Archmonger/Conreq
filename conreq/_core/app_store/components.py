6aa6bbcd125f7a94a0b32f48b2e9b2a4a4ac3017
Archmonger <16909269+Archmonger@users.noreply.github.com>
2022-10-07 00:21:36 -0700

    snazzy loading animation for app store

--- conreq/_core/app_store/components.py
+++ conreq/_core/app_store/components.py
@@ -1,4 +1,5 @@
-from uuid import uuid4
+import asyncio
+import random
 
 from django_idom.components import django_css
 from django_idom.hooks import use_query
@@ -50,7 +51,7 @@ def app_store(state: HomepageState, set_state):
         set_state(state)
 
     # Convert categories to a dictionary for easier rendering
-    if not category_query.loading and not category_query.error:
+    if not category_query.loading and not category_query.error and not categories:
         set_categories(subcategories_to_dict(category_query.data))
 
     # Don't render if there's an error loading categories
@@ -59,7 +60,7 @@ def app_store(state: HomepageState, set_state):
 
     # Don't render if categories are still loading, or if they haven't been converted to a dict yet
     if category_query.loading or not categories:
-        return _(None)
+        return None
 
     # TODO: Update app store entries every first load
     return _(
@@ -68,20 +69,22 @@ def app_store(state: HomepageState, set_state):
         if tab
         else div(
             {"className": "spotlight-region"},
-            our_favorites(set_tab),
-            most_popular(set_tab),
-            recently_updated(set_tab),
-            top_downloaded(set_tab),
-            recently_added(set_tab),
-            essentials(set_tab),
-            random_selection(set_tab),
+            _(
+                our_favorites(set_tab),
+                most_popular(set_tab),
+                recently_updated(set_tab),
+                top_downloaded(set_tab),
+                recently_added(set_tab),
+                essentials(set_tab),
+                random_selection(set_tab),
+            ),
             key="spotlight-region",
         ),
-        div({"className": "nav-region blur"}, nav_constructor(categories, set_tab)),
+        div({"className": "nav-region blur"}, app_store_nav(categories, set_tab)),
     )
 
 
-def nav_constructor(categories: dict[Category, list[Subcategory]], set_tab) -> list:
+def app_store_nav(categories: dict[Category, list[Subcategory]], set_tab) -> list:
     return [
         div(
             {"className": "nav-item"},
@@ -116,45 +119,69 @@ def nav_constructor(categories: dict[Category, list[Subcategory]], set_tab) -> l
     ]
 
 
+@component
 def recently_added(set_tab):
-    return spotlight("Recently Added", "The latest from our community.", set_tab)
+    return _(spotlight("Recently Added", "The latest from our community.", set_tab))
 
 
+@component
 def recently_updated(set_tab):
-    return spotlight(
-        "Recently Updated", "Actively maintained for all to enjoy.", set_tab
+    return _(
+        spotlight("Recently Updated", "Actively maintained for all to enjoy.", set_tab)
     )
 
 
+@component
 def most_popular(set_tab):
-    return spotlight(
-        "Most Popular", "These have gained a serious amount lot of love.", set_tab
+    return _(
+        spotlight(
+            "Most Popular", "These have gained a serious amount lot of love.", set_tab
+        )
     )
 
 
+@component
 def top_downloaded(set_tab):
-    return spotlight("Top Downloaded", "Tons of downloads!", set_tab)
+    return _(spotlight("Top Downloaded", "Tons of downloads!", set_tab))
 
 
+@component
 def our_favorites(set_tab):
-    return spotlight(
-        "Our Favorites", "A curated list of our favorites.", set_tab, special=True
+    return _(
+        spotlight(
+            "Our Favorites", "A curated list of our favorites.", set_tab, special=True
+        )
     )
 
 
+@component
 def essentials(set_tab):
-    return spotlight(
-        "Essentials", "Something we think all users should consider.", set_tab
+    return _(
+        spotlight(
+            "Essentials", "Something we think all users should consider.", set_tab
+        )
     )
 
 
+@component
 def random_selection(set_tab):
-    return spotlight("Random Selection", "Are you feeling lucky today?", set_tab)
+    return _(spotlight("Random Selection", "Are you feeling lucky today?", set_tab))
 
 
+@component
 def spotlight(title, description, set_tab, apps=None, special=False):
+    opacity, set_opacity = hooks.use_state(0)
+
+    @hooks.use_effect(dependencies=[])
+    async def fade_in_animation():
+        await asyncio.sleep(round(random.uniform(0, 0.3), 3))
+        set_opacity(1)
+
     return div(
-        {"className": "spotlight"},
+        {
+            "className": "spotlight fade-in",
+            "style": {"opacity": opacity},
+        },
         a(
             {"href": "#", "onClick": lambda x: print("clicked")},
             h4({"className": "title"}, title),
@@ -162,14 +189,25 @@ def spotlight(title, description, set_tab, apps=None, special=False):
         ),
         div(
             {"className": "card-stage"},
-            [card(set_tab, special) for _ in range(8)],
+            _([card(set_tab, special, key=key) for key in range(8)]),
         ),
     )
 
 
+@component
 def card(set_tab, special, app: PlaceholderApp = PlaceholderApp()):
+    opacity, set_opacity = hooks.use_state(0)
+
+    @hooks.use_effect(dependencies=[])
+    async def fade_in_animation():
+        await asyncio.sleep(round(random.uniform(0, 0.55), 3))
+        set_opacity(1)
+
     return div(
-        {"className": "card" + (" special" if special else "")},
+        {
+            "className": "card fade-in" + (" special" if special else ""),
+            "style": {"opacity": opacity},
+        },
         div(
             {"className": "top"},
             div(
@@ -216,5 +254,4 @@ def card(set_tab, special, app: PlaceholderApp = PlaceholderApp()):
             ),
         ),
         div({"className": "description"}, app.short_description),
-        key=str(uuid4()),
     )
