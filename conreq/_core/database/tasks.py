16b8798585daa997d02a95a6127ccbbbd2d02aca
Archmonger <16909269+Archmonger@users.noreply.github.com>
2023-01-19 03:18:27 -0800

    clearsessions periodic task

--- conreq/_core/database/tasks.py
+++ conreq/_core/database/tasks.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 from django.conf import settings
+from django.core.management import call_command
 from django.db import connection
 from huey import crontab
 from huey.contrib.djhuey import db_periodic_task
@@ -13,7 +14,7 @@ HUEY_FILENAME = getattr(settings, "HUEY_FILENAME")
 
 @db_periodic_task(crontab(minute="0", hour="0", day_of_week="1"))
 def clean_background_task_db():
-    """Periodically performs a SQLITE vacuum on the background task database."""
+    """Periodically performs a SQLITE vacuum on the background task database weekly."""
     with sqlite3.connect(HUEY_FILENAME) as cursor:
         cursor.execute(
             # Only keep the 1000 latest tasks
@@ -37,6 +38,12 @@ if DB_ENGINE == "SQLITE3":
 
     @db_periodic_task(crontab(minute="0", hour="0", day_of_week="1"))
     def clean_conreq_db():
-        """Periodically performs any cleanup tasks needed for the default database."""
+        """Periodically performs any cleanup tasks needed for the default database weekly."""
         with connection.cursor() as cursor:
             cursor.execute("VACUUM")
+
+
+@db_periodic_task(crontab(minute="0", hour="0", day="1"))
+def clean_sessions():
+    """Periodically deletes expired sessions from the database monthly."""
+    call_command("clearsessions")
